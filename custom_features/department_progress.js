e0cb2114c3bfb72b12ac6511213dd8e8f6b42ba8
jhveem <joshua.hveem@gmail.com>
2020-08-11 15:18:48 -0600

    formatting vue html

--- custom_features/department_progress.js
+++ custom_features/department_progress.js
@@ -2,74 +2,76 @@
   let canvasbody = $("#application");
   let vueString = `
 <div>
-<div
-  @mouseover="buttonX = 120;"
-  @mouseleave="buttonX = 10;"
-  @click="show = !show;"
-  style='
-    width: 110px;
-    margin-right: -140px;
-    position:fixed;
-    bottom: 60px;
-    z-index:1000;
-    transition: 0.5s;
-    background-color: #49e;
-    border: 2px solid #5ae;
-    padding: 10px 20px;
-    color: #FFF;
-    border-radius: 5px;
-    cursor: pointer;
-    user-select: none;
-  '
-  :style="{'right': buttonX + 'px'}"
->
-Dept. Status
-</div>
-<div v-if='show' class='btech-modal' style='display: inline-block;'
-@mouseup="dropElement"
->
-<!-- ERASE THE DISPLAY PIECE BEFORE GOING LIVE -->
-<div class='btech-modal-content' style='left: 2.5%; top: 2.5%; height: 95%; width: 95%; position: absolute; box-sizing: border-box;'>
-<div class="btech-tabs" >
-<ul style='background-color: #2d3b45'>
-<li style='float: right;' v-on:click='close()'>X</li>
-</ul>
-</div>
-<div class='btech-modal-content-inner'
-style='height: 100%; position: relative;'
-@mousemove="onMouseMove($event)"
->
-
-
-<div
-v-for='topic in topics'
-style='position: absolute; user-select: none; cursor: pointer; background-color: #49e; color: #fff; padding: 0px .5rem; font-size: 1.5rem; border-radius: 5px;'
-v-bind:style='{left: "calc(" + topic.elX + "%)", top: "calc(" + topic.elY + "%)"}'
->
-<span v-show='topic.editing === false'
-  @mousedown="grabElement($event, topic)"
->
-  {{topic.title}}
-</span>
-<input v-show='topic.editing === true' v-model='topic.title' v-on:blur='topic.editing=false; saveTopicElement(topic);'>
-
-</div>
-
-<div
-v-for='department in departments'
-style='position: absolute; user-select: none; cursor: pointer; border: solid 1px #eee; border-left: solid 1rem #49e; padding: 0px .5rem; background-color: #fff;'
-v-bind:style='{left: "calc(" + department.elX + "%)", top: "calc(" + department.elY + "%)"}'
-@mousedown="grabElement($event, department)"
->
-  <span
-    @mousedown="grabElement($event, department)"
+  <div
+    @mouseover="buttonX = 120;"
+    @mouseleave="buttonX = 10;"
+    @click="show = !show;"
+    style='
+      width: 110px;
+      margin-right: -140px;
+      position:fixed;
+      bottom: 60px;
+      z-index:1000;
+      transition: 0.5s;
+      background-color: #49e;
+      border: 2px solid #5ae;
+      padding: 10px 20px;
+      color: #FFF;
+      border-radius: 5px;
+      cursor: pointer;
+      user-select: none;
+    '
+    :style="{'right': buttonX + 'px'}"
   >
-    {{department.data.name}}
-  </span>
-</div>
-</div>
-</div>
-</div>
+    Dept. Status
+  </div>
+  <div 
+    v-if='show' 
+    class='btech-modal' 
+    style='display: inline-block;'
+    @mouseup="dropElement"
+  >
+    <!-- ERASE THE DISPLAY PIECE BEFORE GOING LIVE -->
+    <div class='btech-modal-content' style='left: 2.5%; top: 2.5%; height: 95%; width: 95%; position: absolute; box-sizing: border-box;'>
+      <div class="btech-tabs" >
+        <ul style='background-color: #2d3b45'>
+          <li style='float: right;' v-on:click='close()'>X</li>
+        </ul>
+      </div>
+
+      <div class='btech-modal-content-inner'
+        style='height: 100%; position: relative;'
+        @mousemove="onMouseMove($event)"
+      >
+        <div
+          v-for='topic in topics'
+          style='position: absolute; user-select: none; cursor: pointer; background-color: #49e; color: #fff; padding: 0px .5rem; font-size: 1.5rem; border-radius: 5px;'
+          v-bind:style='{left: "calc(" + topic.elX + "%)", top: "calc(" + topic.elY + "%)"}'
+        >
+          <span v-show='topic.editing === false'
+            @mousedown="grabElement($event, topic)"
+          >
+            {{topic.title}}
+          </span>
+          <input v-show='topic.editing === true' v-model='topic.title' v-on:blur='topic.editing=false; saveTopicElement(topic);'>
+
+        </div>
+
+        <div
+            v-for='department in departments'
+            style='position: absolute; user-select: none; cursor: pointer; border: solid 1px #eee; border-left: solid 1rem #49e; padding: 0px .5rem; background-color: #fff;'
+            v-bind:style='{left: "calc(" + department.elX + "%)", top: "calc(" + department.elY + "%)"}'
+            @mousedown="grabElement($event, department)"
+        >
+          <span
+            @mousedown="grabElement($event, department)"
+          >
+            {{department.data.name}}
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 `;
   canvasbody.after('<div id="btech-course-status-vue"></div>');
