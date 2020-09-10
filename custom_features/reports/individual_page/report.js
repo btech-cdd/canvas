7eb4d98f110b67aefe11aa13ef5557c6f29ba8d5
jhveem <joshua.hveem@gmail.com>
2020-09-10 08:29:24 -0600

    append instead of prepend

--- custom_features/reports/individual_page/report.js
+++ custom_features/reports/individual_page/report.js
@@ -33,7 +33,7 @@
         if (/^\/$/.test(window.location.pathname)) {
           gen_report_button = $('<a class="btn button-sidebar-wide" id="canvas-individual-report-vue-gen"><i class="icon-gradebook"></i>Courses Report</a>');
           menu_bar = $("#right-side div").last();
-          gen_report_button.prependTo(menu_bar);
+          gen_report_button.appendTo(menu_bar);
         } else if (/^\/courses\/[0-9]+\/users\/[0-9]+$/.test(window.location.pathname)) {
           gen_report_button = $('<a style="cursor: pointer;" id="canvas-individual-report-vue-gen"><i class="icon-gradebook"></i> Courses Report</a>');
           menu_bar = $("#right-side div").first();
