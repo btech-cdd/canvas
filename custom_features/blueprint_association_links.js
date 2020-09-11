36b0fb1def232136a9958f87b277813021a10d4a
jhveem <joshua.hveem@gmail.com>
2020-09-11 13:58:08 -0600

    remove cling

--- custom_features/blueprint_association_links.js
+++ custom_features/blueprint_association_links.js
@@ -1,16 +1,11 @@
 (async function () {
   let blueprintIcon = await getElement(".bcs__trigger")
-  console.log(blueprintIcon);
   blueprintIcon.click(async function (e) {
-    console.log('click!')
     let button = await getElement(".bcs__row__associations button");
-    console.log("READY");
     button.click(async function (e) {
-      console.log('click!!')
       rows = await getElement(".bca-associations-table tr");
       rows.each(function () {
         let spans = $(this).find("td span");
-        console.log(spans);
         if (spans.length > 0) {
           let courseId = $(this).attr("id").replace("course_", "");
           $(spans[0]).wrapInner("<a href='/courses/" + courseId + "' target='#'></a>");
