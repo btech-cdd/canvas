b7f97b07b0105e827da41f8f8202c80d3804a9d4
jhveem <joshua.hveem@gmail.com>
2020-09-15 13:52:55 -0600

    cling

--- custom_features/reports/accredidation-beta.js
+++ custom_features/reports/accredidation-beta.js
@@ -234,8 +234,8 @@
               $("#content").append("<div id='test-export-" + id + "'></div>");
               $("#test-export-" + id).append(document.getElementById('btech-content-' + id).contentWindow.document.getElementById('rubric_holder'));
               html2canvas(document.querySelector('#test-export-' + id)).then(canvas => {
+                console.log(canvas);
                 canvas.toBlob(function (blob) {
-                  console.log(blob);
                   submission.blob = blob;
                 });
               });
