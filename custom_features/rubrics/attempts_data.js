ab844a09d19f2c2b9a702844a63bb14d3b5d8357
jhveem <joshua.hveem@gmail.com>
2020-09-21 12:53:03 -0600

    different hook for speedgrader and vanilla

--- custom_features/rubrics/attempts_data.js
+++ custom_features/rubrics/attempts_data.js
@@ -1,5 +1,5 @@
 console.log("ATTEMPTS");
-(function() {
+(function () {
   IMPORTED_FEATURE = {};
   let rWindowSpeedGrader = /^\/courses\/[0-9]+\/gradebook\/speed_grader/;
   let rWindowVanilla = /^\/courses\/[0-9]+\/assignments\/[0-9]+\/submissions\/[0-9]+/;
@@ -8,7 +8,7 @@ console.log("ATTEMPTS");
       attempts: 0,
       initiated: false,
       commentSelector: "",
-      _init(params={}) {
+      _init(params = {}) {
         let feature = this;
         if (rWindowSpeedGrader.test(window.location.pathname)) {
           feature.commentSelector = "div.comment span.comment, tr.comments";
@@ -17,7 +17,7 @@ console.log("ATTEMPTS");
           feature.commentSelector = "div.comment div.comment span";
         }
         feature.insertAttemptsData();
-        $(".save_rubric_button").on("click", function() {
+        $(".save_rubric_button").on("click", function () {
           console.log("save data");
           feature.calcAttemptsData();
           feature.attempts += 1;
@@ -26,7 +26,13 @@ console.log("ATTEMPTS");
       async insertAttemptsData() {
         console.log("Add Div");
         let feature = this;
-        let details = await getElement("#grade_container");
+        let details;
+        if (rWindowSpeedGrader.test(window.location.pathname)) {
+          details = await getElement("#grade_container");
+        }
+        if (rWindowVanilla.test(window.location.pathname)) {
+          details = await getElement(".grade-values");
+        }
         details.after(
           `<tr id="btech-attempts-data" class="content_box">
           <td colspan="3">
@@ -41,20 +47,21 @@ console.log("ATTEMPTS");
         let feature = this;
         feature.attempts = 0;
         let elements = await getElement(feature.commentSelector);
-        elements.each(function() {
-            let checkAttempt = $(this).html().includes("RUBRIC");
-            if (checkAttempt) {
-                feature.attempts += 1;
-            }
+        elements.each(function () {
+          let checkAttempt = $(this).html().includes("RUBRIC");
+          if (checkAttempt) {
+            feature.attempts += 1;
+          }
         });
         if (feature.attempts > 0) {
           let rubricTotalText = $("[data-selenium='rubric_total']").text();
+          console.log(rubricTotalText);
           let match = rubricTotalText.match(/([0-9]+)/g);
           rubricTotal = parseInt(match[0]);
           rubricMax = parseInt(match[1]);
           let suggestedScore = Math.round(rubricTotal * ((11 - feature.attempts) / 10));
           $("#btech-recorded-attempts-value").text(feature.attempts);
-          $("#btech-rubric-score-value").text(rubricTotal + " ("+ (Math.round((rubricTotal / rubricMax) * 1000) / 10)+"%)");
+          $("#btech-rubric-score-value").text(rubricTotal + " (" + (Math.round((rubricTotal / rubricMax) * 1000) / 10) + "%)");
           $("#btech-suggested-score-value").text(suggestedScore);
         }
       }
