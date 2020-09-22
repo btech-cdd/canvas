0f1b83fe1a6f9652ac0439eddbb19529b5ff17ae
jhveem <joshua.hveem@gmail.com>
2020-09-22 16:25:10 -0600

    save for testing

--- custom_features/reports/accredidation-beta.js
+++ custom_features/reports/accredidation-beta.js
@@ -324,6 +324,7 @@
                 doc.rect(0, pageHeight - padding, pageWidth, padding, 'F');
                 heightLeft -= imgHeight;
               }
+              doc.save('test.pdf');
               return doc.output('blob');
             },
             async getBlobQuiz(assignment, submission) {
