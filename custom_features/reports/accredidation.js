021949df850c349c43ae48c3c3120d92ca6198a6
Joshua Hveem <joshua.hveem@gmail.com>
2020-09-30 14:55:59 -0600

    tweaking css

--- custom_features/reports/accredidation.js
+++ custom_features/reports/accredidation.js
@@ -185,7 +185,7 @@
             content.prepend("<div>Submitted:" + data.submission.submitted_at + "</div>");
             content.prepend("<div>Student:" + data.submission.user.name + "</div>");
             content.prepend("<div>Assignment:" + data.assignment.name + "</div>");
-            // content.append(commentEl);
+            content.append(commentEl);
             let ogTitle = $('title').text();
             $('title').text(title);
             let window = document.getElementById(elId).contentWindow;
