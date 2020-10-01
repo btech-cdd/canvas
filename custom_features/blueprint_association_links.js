65b03c42de086102434914b4f11a8fcca1305403
Joshua Hveem <joshua.hveem@gmail.com>
2020-10-01 09:28:41 -0600

    attempting jquery event delegation

--- custom_features/blueprint_association_links.js
+++ custom_features/blueprint_association_links.js
@@ -1,21 +1,16 @@
 (async function () {
   console.log("BLUEPRINT LINKS");
-  let blueprintIcon = await getElement(".bcs__trigger")
-  blueprintIcon.click(async function (e) {
-    console.log("BUTTON CLICK")
-    let sideBar = await getElement(".bcs__row__associations");
-    sideBar.on('click', 'button', async function(e) {
-      console.log("BUTTON 2 CLICK")
-      rows = await getElement(".bca-associations-table tr");
-      rows.each(function () {
-        console.log("ROW FOUND");
-        let spans = $(this).find("td span");
-        if (spans.length > 0) {
-          let courseId = $(this).attr("id").replace("course_", "");
-          console.log(courseId);
-          $(spans[0]).wrapInner("<a href='/courses/" + courseId + "' target='#'></a>");
-        }
-      });
+  $('body').on('click', '.bcs__row__associations button', async function (e) {
+    console.log("BUTTON 2 CLICK")
+    rows = await getElement(".bca-associations-table tr");
+    rows.each(function () {
+      console.log("ROW FOUND");
+      let spans = $(this).find("td span");
+      if (spans.length > 0) {
+        let courseId = $(this).attr("id").replace("course_", "");
+        console.log(courseId);
+        $(spans[0]).wrapInner("<a href='/courses/" + courseId + "' target='#'></a>");
+      }
     });
   });
 })();
