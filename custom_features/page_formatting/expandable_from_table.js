9e80dfebe17b6becc41fe03c51fe04992c576c29
jhveem <joshua.hveem@gmail.com>
2020-10-22 14:25:17 -0600

    add expandable table

---
+++ custom_features/page_formatting/expandable_from_table.js
@@ -0,0 +1,38 @@
+(function () {
+  let originalTables = $(".btech-expandable-table");
+  originalTables.each(function () {
+    let originalTable = $(this);
+    let newTable = $("<div class='btech-expandable-container btech-expandable'></div>");
+    let rows = originalTable.find("> tbody > tr");
+    let data = {};
+    let caption = originalTable.find("caption").html();
+    if (caption !== null) {
+      newTable.prepend("<div style='width: 100%; text-align: center;'>" + caption + "</div><br>");
+    }
+    rows.each(function () {
+      let row = $(this);
+      let cells = row.find("td");
+      if (cells.length > 0) {
+        let tab = $(cells[0]).text();
+        let h3 = $("<h3 class='btech-toggler'>" + tab + "</h3>");
+        data[tab] = $(cells[1]).html();
+        newTable.append(h3);
+        let content = $("<div></div>");
+        content.append(data[tab]);
+        content.hide();
+        h3.after(content);
+        h3.click(function () {
+          content.toggle();
+        });
+      }
+    });
+    originalTable.before(newTable);
+    //set up custom themes from theme parent if needed
+    let themeParent = $('#btech-theme-parent');
+    if (themeParent.length === 1) {
+      //newTableTabs.css({'background-color': themeParent.css('background-color')});
+    }
+    //newTableContent.hide();
+    originalTable.hide();
+  });
+})();
