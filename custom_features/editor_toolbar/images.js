b853dc710a4e1eb03244a103ee3d8976e4c9108b
jhveem <joshua.hveem@gmail.com>
2020-10-27 12:10:27 -0600

    move image buttons to own file

---
+++ custom_features/editor_toolbar/images.js
@@ -0,0 +1,67 @@
+
+(async function () {
+  if (!TOOLBAR.checkEditorPage()) return;
+  function resetImgButtons() {
+    let node = tinyMCE.activeEditor.selection.getNode();
+    let parent = tinyMCE.activeEditor.dom.getParent(node, "img");
+    $('#btech-custom-editor-select-tables :nth-child(1)').prop('selected', true)
+    let found = false;
+    $('.btech-table-edit-option').each(function () {
+      $(this).css({
+        'background-color': '#eee',
+        'color': '#000'
+      });
+      let className = $(this).attr('id').replace("-option", "");
+      if (parent !== null) {
+        if ($(parent).hasClass(className)) {
+          let bgColor = getComputedStyle(document.documentElement, null).getPropertyValue("--ic-brand-button--secondary-bgd-darkened-5");
+          $(this).css({
+            'background-color': bgColor,
+            'color': '#fff'
+          });
+          //also set this option to the selected option
+          //this isn't working at the moment
+          $(this).prop('selected', true);
+          found = true;
+        }
+      }
+    });
+  async function addClassToImage(className) {
+    //get the currently selected node
+    let node = tinyMCE.activeEditor.selection.getNode();
+    //get the parent
+    let parent = tinyMCE.activeEditor.dom.getParent(node, "img");
+    if (parent !== null) {
+      //see if it's already got the class in question, if so remove it, otherwise remove all other classes and add that one
+      if ($(parent).hasClass(className)) {
+        tinyMCE.activeEditor.dom.removeClass(parent, className);
+      } else {
+        for (let i = 0; i < tableOptions.length; i++) {
+          let _class = tableOptions[i];
+          tinyMCE.activeEditor.dom.removeClass(parent, _class);
+        }
+        tinyMCE.activeEditor.dom.addClass(parent, className);
+      }
+    }
+    return parent;
+  }
+
+  function blurGraphicImage() {
+    addClassToImage('btech-graphic-image');
+  }
+
+  function setImageAlign() {
+    addClassToImage('btech-img-align');
+  }
+    //if no options are selected, select the disabled or default option
+    //this isn't working at the moment
+    if (!found) {
+      $('.btech-table-edit-option:disabled').select();
+    }
+  }
+  TOOLBAR.addButtonIcon("far fa-exclamation-triangle", "Blur image with graphic content.", blurGraphicImage);
+  TOOLBAR.addButtonIcon("far fa-arrows-alt-h", "Set image to appear on same row as images adjacent to it.", setImageAlign);
+  tinymce.activeEditor.on("click", function () {
+    resetImageButtons();
+  });
+})();
