e2554b8ed0e4a0844910a99bdedfacd7caac66e7
jhveem <joshua.hveem@gmail.com>
2020-10-28 15:24:59 -0600

    save assigned to

--- custom_features/flag_for_review.js
+++ custom_features/flag_for_review.js
@@ -100,9 +100,9 @@ Look into quill editor
         <div style='text-align: right;'><i>-{{name(flag.createdBy)}}</i></div>
         <div style='text-align: right;'>
           <i class='far fa-share-square'></i>
-          <select>
+          <select @change='saveFlagEdits(flag, "assignedTo")'>
             <option value='' selected disabled>-Assign To-</option>
-            <option v-for='(id, name) in loadedNames'>
+            <option v-for='(id, name) in loadedNames' :value='id' v-model='flag.assignedTo[0]'>
               {{id}}
             </option>
           </select>
