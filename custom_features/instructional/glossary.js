1d7878a1fb7ee479dfeef29addd7a193b1174b53
jhveem <joshua.hveem@gmail.com>
2020-11-09 12:25:22 -0700

    include pre and post char

--- custom_features/instructional/glossary.js
+++ custom_features/instructional/glossary.js
@@ -78,9 +78,9 @@
           let html = p.html();
           for (let t = 0; t < feature.terms.length; t++) {
             let term = feature.terms[t];
-            let regEx = new RegExp('[^a-zA-Z0-9](' + term + ')[^a-zA-Z0-9]', 'ig');
+            let regEx = new RegExp('([^a-zA-Z0-9])(' + term + ')([^a-zA-Z0-9])', 'ig');
             let cssTerm = term.replace(' ', '-').toLowerCase();
-            let replace = "<span style='font-weight: bold; cursor: help;' class='btech-glossasry-inline-definition btech-glossary-term-" + cssTerm + "'>$1</span>";
+            let replace = "$1<span style='font-weight: bold; cursor: help;' class='btech-glossasry-inline-definition btech-glossary-term-" + cssTerm + "'>$2</span>$3";
             html = html.replace(regEx, replace);
           }
           p.html(html);
