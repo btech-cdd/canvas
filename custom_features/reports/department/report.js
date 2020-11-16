e296e53228d077a750777aecdbc718b3bacc60f6
Joshua Hveem <joshua.hveem@gmail.com>
2020-11-16 14:47:01 -0700

    cling

--- custom_features/reports/department/report.js
+++ custom_features/reports/department/report.js
@@ -1,8 +1,10 @@
 (async function () {
   $('#content').empty();
   let dept = CURRENT_DEPARTMENT_ID;
+  console.log(dept);
   let jsonUrl = 'https://jhveem.xyz/canvas/custom_features/reports/department/data.json';
   let jsonData = await canvasGet(jsonUrl);
+  console.log(jsonData);
   let deptData = jsonData[dept];
   console.log(deptData);
   let enrollmentsUrl = '/api/v1/accounts/' + dept + '/users';
