6bb71b806d8918bf4c0d888f24c5d95e3d9bd44f
Joshua Hveem <joshua.hveem@gmail.com>
2020-11-16 15:49:20 -0700

    change data name

--- custom_features/reports/department/report.js
+++ custom_features/reports/department/report.js
@@ -3,7 +3,7 @@
   let dept = CURRENT_DEPARTMENT_ID;
   let jsonUrl = 'https://jhveem.xyz/canvas/custom_features/reports/department/data.json';
   let jsonData = await canvasGet(jsonUrl);
-  let deptData = jsonData[0][dept];
+  let deptUsers = jsonData[0][dept];
   let usersUrl = '/api/v1/accounts/' + dept + '/users';
   let users = await canvasGet(usersUrl, {
     enrollment_type: 'student'
@@ -11,11 +11,12 @@
   console.log('awaited');
   for (let i = 0; i < users.length; i++) {
     let user = users[i];
-    if (user.id in deptData) {
-      deptData[user.id].name = user.sortable_name;
+    if (user.id in deptUsers) {
+      deptUsers[user.id].name = user.sortable_name;
     }
   }
-  for (let user in deptData) {
+  for (let userId in deptUsers) {
+    let user = deptUsers[userId];
     console.log(user);
   }
 })();
