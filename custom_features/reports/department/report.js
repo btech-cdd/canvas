f2ec009818f56af6f8f676d9ca9ebe75af63d98a
jhveem <joshua.hveem@gmail.com>
2020-11-18 09:58:34 -0700

    change report into vue

--- custom_features/reports/department/report.js
+++ custom_features/reports/department/report.js
@@ -1,36 +1,74 @@
 //include a last updated list
 (async function () {
-  let content = $('#content');
-  content.empty();
-  let dept = CURRENT_DEPARTMENT_ID;
-  let jsonUrl = 'https://jhveem.xyz/canvas/custom_features/reports/department/progress.json';
-  let jsonData = await canvasGet(jsonUrl);
-  let deptUsers = jsonData[0][dept];
-  let usersUrl = '/api/v1/accounts/' + dept + '/users';
-  let users = await canvasGet(usersUrl, {
-    enrollment_type: 'student'
-  });
-  console.log('awaited');
-  for (let i = 0; i < users.length; i++) {
-    let user = users[i];
-    if (user.id in deptUsers) {
-      deptUsers[user.id].name = user.sortable_name;
-    }
-  }
-  for (let userId in deptUsers) {
-    let user = deptUsers[userId];
-    let div = $('<div></div>');
-    div.append('<span><a href="/users/'+userId+'">' + user.name + '</a></span><br>');
-    for (let courseCode in user.courses) {
-      let course = user.courses[courseCode][0];
-      let progress = course.progress;
-      if (progress > 100) progress = 100;
-      div.append(`<div style="display: inline-block; border: 1px solid #000; background-color: #334;">
+  IMPORTED_FEATURE = {};
+  IMPORTED_FEATURE = {
+    initiated: false,
+    async _init(params = {}) {
+      let vueString = '';
+      await $.get(SOURCE_URL + '/custom_features/reports/department/template.vue', null, function (html) {
+        vueString = html.replace("<template>", "").replace("</template>", "");
+      }, 'text');
+      let content = $("#content");
+      content.empty();
+      content.append('<div id="canvas-department-report-vue"></div>')
+      $("#canvas-department-report-vue").append(vueString);
+      this.APP = new Vue({
+        el: '#canvas-individual-report-vue',
+        mounted: async function () {
+          let app = this;
+          let dept = CURRENT_DEPARTMENT_ID;
+          await app.loadJsonFile('progress');
+          await app.loadJsonFile('departments');
+          await app.loadJsonFile('canvas_to_jenz');
+
+          let deptUsers = app.json['progress'][0][CURRENT_DEPARTMENT_ID];
+          let usersUrl = '/api/v1/accounts/' + dept + '/users';
+          let users = await canvasGet(usersUrl, {
+            enrollment_type: 'student'
+          });
+          for (let i = 0; i < users.length; i++) {
+            let user = users[i];
+            if (user.id in deptUsers) {
+              deptUsers[user.id].name = user.sortable_name;
+            }
+          }
+          app.users = deptUsers;
+          /*
+          for (let userId in deptUsers) {
+            let user = deptUsers[userId];
+            let div = $('<div></div>');
+            div.append('<span><a href="/users/' + userId + '">' + user.name + '</a></span><br>');
+            for (let courseCode in user.courses) {
+              let course = user.courses[courseCode][0];
+              let progress = course.progress;
+              if (progress > 100) progress = 100;
+              div.append(`<div style="display: inline-block; border: 1px solid #000; background-color: #334;">
           <div style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; background-color: #1C91A4; color: #fff; width: ` + progress + `%;'>` + courseCode + `</div>
         </div>`);
+            }
+            div.append('<br>')
+            content.append(div);
+            console.log(user);
+          }
+          */
+          app.loading = false;
+        },
+
+        data: function () {
+          return {
+            loading: true,
+            json: {},
+            users: [],
+          }
+        },
+        methods: {
+          async loadJsonFile(name) {
+            let jsonUrl = 'https://jhveem.xyz/canvas/custom_features/reports/department/'+name+'.json';
+            let jsonData = await canvasGet(jsonUrl);
+            json[name] = jsonData;
+          }
+        }
+      })
     }
-    div.append('<br>')
-    content.append(div);
-    console.log(user);
   }
 })();
---
+++ custom_features/reports/department/template.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div 
+      v-for='user in users'
+      :key='user'
+    >
+      <span><a :href="'/users/' + userId">{{user.name}}</a></span><br>
+      <div style="display: inline-block; border: 1px solid #000; background-color: #334;">
+
+        <div 
+          v-for='course in user.courses'
+          v-bind:key='course'
+          style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; background-color: #1C91A4; color: #fff; width: ` + progress + `%;'
+         >
+          {{course}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
