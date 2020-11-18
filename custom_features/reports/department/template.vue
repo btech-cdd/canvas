03c6e4ee096bfeffece3e668809784e9227a6e76
jhveem <joshua.hveem@gmail.com>
2020-11-18 11:18:12 -0700

    testing

--- custom_features/reports/department/template.vue
+++ custom_features/reports/department/template.vue
@@ -11,14 +11,13 @@
       <div 
         v-for='course in coreCourses'
         :key='course.code'
-        :courseData='0'
         style="display: inline-block; border: 1px solid #000; background-color: #334;"
       >
         <div 
           style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; background-color: #1C91A4; color: #fff;'
           :style="
             {
-              width: courseData + '%'
+              width: user.courses[course.code][0] + '%'
             }
           " 
          >
