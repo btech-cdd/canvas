aebf533c258ab4d04a4688a8f2f56f3d83e32d35
jhveem <joshua.hveem@gmail.com>
2020-11-18 10:10:02 -0700

    key and val for v-for

--- custom_features/reports/department/template.vue
+++ custom_features/reports/department/template.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
     <div 
-      v-for='user in users'
-      :key='user'
+      v-for='(user, userId) in users'
+      :key='userId'
     >
       <span><a :href="'/users/' + userId">{{user.name}}</a></span><br>
       <div style="display: inline-block; border: 1px solid #000; background-color: #334;">
 
         <div 
-          v-for='course in user.courses'
-          v-bind:key='course'
+          v-for='(course, courseId) in user.courses'
+          v-bind:key='courseId'
           style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; background-color: #1C91A4; color: #fff; width: ` + progress + `%;'
          >
           {{course}}
