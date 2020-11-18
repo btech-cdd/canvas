109aafc6c88f2458223d530e0f3d808c2e5f91d1
jhveem <joshua.hveem@gmail.com>
2020-11-18 11:13:12 -0700

    display all core courses

--- custom_features/reports/department/template.vue
+++ custom_features/reports/department/template.vue
@@ -11,8 +11,19 @@
       <div 
         v-for='course in coreCourses'
         :key='course.code'
+        :courseData='user.courses[course.code][0]'
+        style="display: inline-block; border: 1px solid #000; background-color: #334;"
       >
-        {{course.code}}
+        <div 
+          style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; background-color: #1C91A4; color: #fff;'
+          :style="
+            {
+              width: courseData.progress + '%'
+            }
+          " 
+         >
+          {{course.code}}
+        </div>
       </div>
       <div 
         v-for='(course, courseId) in user.courses'
