68bcf35a9e14e7c1a3ecc8d63ccf1557ec14d210
jhveem <joshua.hveem@gmail.com>
2020-11-19 11:39:58 -0700

    move closing tag to include everything

--- custom_features/reports/department/template.vue
+++ custom_features/reports/department/template.vue
@@ -25,27 +25,27 @@
             {{course.code}}
           </div>
         </div>
-      </div>
 
-      <!--Elective-->
-      <div>Electives</div>
-      <div v-for='course in electiveCourses' :key='course.code'
-        style="display: inline-block; border: 1px solid #000; background-color: #334;">
-        <div
-          style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; background-color: #1C91A4; color: #fff;'
-          v-if='user.courses[course.code] !== undefined' :style="
+        <!--Elective-->
+        <div>Electives</div>
+        <div v-for='course in electiveCourses' :key='course.code'
+          style="display: inline-block; border: 1px solid #000; background-color: #334;">
+          <div
+            style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; background-color: #1C91A4; color: #fff;'
+            v-if='user.courses[course.code] !== undefined' :style="
             {
               width: user.courses[course.code][0] + '%'
             }
           ">
-          {{course.code}}
+            {{course.code}}
+          </div>
+          <div style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; color: #fff;'
+            v-else>
+            {{course.code}}
+          </div>
         </div>
-        <div style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; color: #fff;'
-          v-else>
-          {{course.code}}
-        </div>
-      </div>
 
+      </div>
     </div>
   </div>
 </template>
