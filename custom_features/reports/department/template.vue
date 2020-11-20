30320e8ece44ab89689a3b20b1e325ee4fe3fd9a
jhveem <joshua.hveem@gmail.com>
2020-11-20 13:42:56 -0700

    fix progress display bar

--- custom_features/reports/department/template.vue
+++ custom_features/reports/department/template.vue
@@ -33,7 +33,7 @@
               style='box-sizing: border-box; white-space: nowrap; padding: 0px 5px; font-size: 0.75em; background-color: #1C91A4; color: #fff;'
               v-if='user.courses[course.code] !== undefined' :style="
             {
-              width: user.courses[course.code][0] + '%'
+              width: user.courses[course.code][0].progress + '%'
             }
           ">
               {{course.code}}
