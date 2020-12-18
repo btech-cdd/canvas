eb6fdcc31e96bc3498b7f6bfb50e6a0e29d60cd3
jhveem <joshua.hveem@gmail.com>
2020-12-18 13:09:07 -0700

    fixing set up of submission dates

--- custom_features/reports/department/report.js
+++ custom_features/reports/department/report.js
@@ -442,6 +442,7 @@
 
       //Begin setting up the graph
       let barColor = graph.getBarColor();
+      console.log(graphElId);
       $('#' + graphElId).empty();
 
       var width = w - graph.graphSettings.margin.left - graph.graphSettings.margin.right;
