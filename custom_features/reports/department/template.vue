847a1822b84cdd2dbdabfb33e924425225c3811f
jhveem <joshua.hveem@gmail.com>
2021-01-05 10:54:20 -0700

    cling

--- custom_features/reports/department/template.vue
+++ custom_features/reports/department/template.vue
@@ -42,7 +42,7 @@
                 <div 
                   v-for='(course) in user[courseType]' 
                   class='btech-course-progress-bar' 
-                  :title="json['progress'][currentDepartment]['base'][course.code].name"
+                  :title="currentDepartment"
                   :key='course.code' 
                   :style="
                     {
