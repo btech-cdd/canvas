57c8b2920db1e4fda4402da0af2bc8d6a33c1230
jhveem <joshua.hveem@gmail.com>
2021-01-11 13:42:43 -0700

    fleshing out individual report

--- lti/department_report/report.js
+++ lti/department_report/report.js
@@ -437,7 +437,7 @@
       return;
     }
 
-    async _init(app, userId, sisId, graphElId = 'btech-department-report-student-submissions-graph', w = 800, h = 450) {
+    async _init(app, userId, sisId, graphElId = 'btech-department-report-student-submissions-graph', w = 800, h = 160) {
       console.log("LOADING?")
       this.app = app;
       let graph = this;
