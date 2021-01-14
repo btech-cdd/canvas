ac8a49bba3d06435a961af129bde6458b83dd7bb
jhveem <joshua.hveem@gmail.com>
2021-01-14 13:33:35 -0700

    fix labels

--- lti/department_report/report.js
+++ lti/department_report/report.js
@@ -253,7 +253,7 @@
         if (user.completedHours === undefined || user.completedHours === 0) return app.colors.gray;
         let timePerc = Math.round((user.enrolledHours / user.completedHours) * 100);
         if (timePerc <= 100) return app.colors.green;
-        if (timePerc < 150) return app.colors.yellow;
+        if (timePerc <= 150) return app.colors.yellow;
         return app.colors.red;
       },
 
