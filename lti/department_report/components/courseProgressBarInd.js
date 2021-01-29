0384f836ad2f227b59d3dc173a65bf8514442d8c
jhveem <joshua.hveem@gmail.com>
2021-01-29 11:14:28 -0700

    fixing course progress bar ind temp

--- lti/department_report/components/courseProgressBarInd.js
+++ lti/department_report/components/courseProgressBarInd.js
@@ -1,26 +1,26 @@
 Vue.component('course-progress-bar-ind', {
   template:` 
    <div class="btech-course-progress-bar" style="width: 20rem;" :style="{
-                    'background-color': bgColor,
-                    'width': ((90 / 90) * 20) + 'rem'
-                  }">
-                  <div 
-                    class="btech-course-progress-bar-fill" 
-                    :style="
-                    {
-                      'background-color': barColor,
-                      'width': progress + '%'
-                    }
-                    ">
-                  </div>
-                  <div style="color: #000000" class="btech-course-progress-bar-text">
-                    {{Math.round(progress * 10) / 10}}% 
-                    <span
-                      v-if="!isNaN(course.hours)">
-                      ({{Math.round((course.hours * progress) / 100)}}/{{course.hours}} HRS)
-                    </span>
-                  </div>
-                </div> 
+      'background-color': bgColor,
+      'width': ((90 / 90) * 20) + 'rem'
+    }">
+    <div 
+      class="btech-course-progress-bar-fill" 
+      :style="
+      {
+        'background-color': barColor,
+        'width': progress + '%'
+      }
+      ">
+    </div>
+    <div style="color: #000000" class="btech-course-progress-bar-text">
+      {{Math.round(progress * 10) / 10}}% 
+      <span
+        v-if="hours!==0">
+        ({{Math.round((hours * progress) / 100)}}/{{hours}} HRS)
+      </span>
+    </div>
+  </div> 
   `,
   props: {
     progress: 0,
