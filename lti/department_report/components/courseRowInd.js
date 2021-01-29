9aac7a5a387f0287c4005dafa918abb55d524e82
jhveem <joshua.hveem@gmail.com>
2021-01-29 11:45:54 -0700

    course row comp

--- lti/department_report/components/courseRowInd.js
+++ lti/department_report/components/courseRowInd.js
@@ -1,24 +1,26 @@
 Vue.component('course-row-ind', {
   template:` 
-    <div
-      style="background-color: rgb(255, 255, 255); display: inline-block; width: 100%; font-size: 0.75rem; padding: .25rem;"
-    >
-      <div style="display: inline-block; width: 15rem;">
-        <a :class="{
-            disabled: courseCanvasId === null
-          }" 
-          style="text-decoration: none; color: #000000;"
-          :href="'https://btech.instructure.com/courses/' + courseCanvasId + '/grades/' + userCanvasId"
-          target="_blank">
-          {{courseName}} ({{courseCode}})
-        </a>
+    <div>
+      <div
+        style="background-color: rgb(255, 255, 255); display: inline-block; width: 100%; font-size: 0.75rem; padding: .25rem;"
+      >
+        <div style="display: inline-block; width: 15rem;">
+          <a :class="{
+              disabled: courseCanvasId === null
+            }" 
+            style="text-decoration: none; color: #000000;"
+            :href="'https://btech.instructure.com/courses/' + courseCanvasId + '/grades/' + userCanvasId"
+            target="_blank">
+            {{courseName}} ({{courseCode}})
+          </a>
+        </div>
+        <!--Change the first 90 under width to the course's hours once figure out how to include that-->
+        <course-progress-bar-ind
+          :progress='progress'
+          :colors='colors'
+          :hours='courseHours'
+        ></course-progress-bar-ind> 
       </div>
-      <!--Change the first 90 under width to the course's hours once figure out how to include that-->
-      <course-progress-bar-ind
-        :progress='progress'
-        :colors='colors'
-        :hours='courseHours'
-      ></course-progress-bar-ind> 
     </div>
   `,
   props: [
