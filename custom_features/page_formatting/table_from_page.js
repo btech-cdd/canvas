4d5438a36963cbbfac1f519e79c926f1ec9263a4
jhveem <joshua.hveem@gmail.com>
2021-02-03 09:31:45 -0700

    copying style from source table

--- custom_features/page_formatting/table_from_page.js
+++ custom_features/page_formatting/table_from_page.js
@@ -1,58 +1,61 @@
-function dupStyle(to, from) {
-  to.attr('style', from.attr('style'));
-  // to.attr('border', from.attr('border'));
-}
-let childTables = $('.btech-table-from-page');
-if (childTables.length > 0) {
-  console.log("CHILD EXISTS");
-  let courseId = ENV.COURSE_ID;
-  $.get('/api/v1/courses/'+courseId+'/pages/parts-list-master', function (data) {
-    console.log("SOURCE FOUND");
-    let pBody = $('<div class=".page-body">' + data.body + '</div>');
-    let sourceTable = pBody.find('.table-from-page-source');
-    rowRef = {};
-    //set style of new table to style of source table
-    sourceRows = sourceTable.find('tbody tr');
-    sourceRows.each(function() {
-      let row = $(this);
-      let key = $(row.find('td')[0]).text().toLowerCase();
-      console.log(key);
-      rowRef[key] = row;
-    });
+(function () {
+  function dupStyle(to, from) {
+    to.attr('style', from.attr('style'));
+    // to.attr('border', from.attr('border'));
+  }
 
-    childTables.each(function() {
-      //Set up table
-      let newTable = $('<table></table>');
-      dupStyle(newTable, sourceTable);
-      console.log("CREATE TABLE")
-      console.log(newTable);
-
-      //pull head directly from source
-      let thead = sourceTable.find('thead').clone();
-      newTable.append(thead);
-      console.log(thead);
-
-      //Create a new body and duplicate style from source
-      let tbody = $("<tbody></tbody>")
-      // dupStyle(tbody, sourceTable.find('tbody'));
-      newTable.append(tbody);
-      console.log(tbody);
-
-      //Go row by from and add in same order as what appears on page
-      let childTable = $(this);
-      let childRows = childTable.find('tbody tr');
-      childRows.each(function() {
+  let childTables = $('.btech-table-from-page');
+  if (childTables.length > 0) {
+    console.log("CHILD EXISTS");
+    let courseId = ENV.COURSE_ID;
+    $.get('/api/v1/courses/' + courseId + '/pages/parts-list-master', function (data) {
+      console.log("SOURCE FOUND");
+      let pBody = $('<div class=".page-body">' + data.body + '</div>');
+      let sourceTable = pBody.find('.table-from-page-source');
+      rowRef = {};
+      //set style of new table to style of source table
+      sourceRows = sourceTable.find('tbody tr');
+      sourceRows.each(function () {
         let row = $(this);
         let key = $(row.find('td')[0]).text().toLowerCase();
         console.log(key);
-        if (key in rowRef) {
-          tbody.append(rowRef[key].clone());
-        }
+        rowRef[key] = row;
       });
 
-      //add the newly created table to the page
-      console.log("ADD TO PAGE");
-      $('.show-content').append(newTable);
-    })
-  });
-}
+      childTables.each(function () {
+        //Set up table
+        let newTable = $('<table></table>');
+        dupStyle(newTable, sourceTable);
+        console.log("CREATE TABLE")
+        console.log(newTable);
+
+        //pull head directly from source
+        let thead = sourceTable.find('thead').clone();
+        newTable.append(thead);
+        console.log(thead);
+
+        //Create a new body and duplicate style from source
+        let tbody = $("<tbody></tbody>")
+        // dupStyle(tbody, sourceTable.find('tbody'));
+        newTable.append(tbody);
+        console.log(tbody);
+
+        //Go row by from and add in same order as what appears on page
+        let childTable = $(this);
+        let childRows = childTable.find('tbody tr');
+        childRows.each(function () {
+          let row = $(this);
+          let key = $(row.find('td')[0]).text().toLowerCase();
+          console.log(key);
+          if (key in rowRef) {
+            tbody.append(rowRef[key].clone());
+          }
+        });
+
+        //add the newly created table to the page
+        console.log("ADD TO PAGE");
+        $('.show-content').append(newTable);
+      })
+    });
+  }
+})();
