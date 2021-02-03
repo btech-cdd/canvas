b7f89e216d7e149448f0dc75c39895c9fe59e573
jhveem <joshua.hveem@gmail.com>
2021-02-03 09:08:00 -0700

    cling key

--- custom_features/page_formatting/table_from_page.js
+++ custom_features/page_formatting/table_from_page.js
@@ -4,6 +4,16 @@ if (childTables.length > 0) {
   $.get('/api/v1/courses/'+courseId+'/pages/parts-list-master', function (data) {
     let pBody = $('<div class=".page-body">' + data.body + '</div>');
     let sourceTable = pBody.find('.table-from-page-source');
+    let newTable = $('<table></table>');
+    rowRef = {};
+    //set style of new table to style of source table
+    let thead = sourceTable.find('thead');
+    rows = sourceTable.find('tbody tr');
+    rows.each(function() {
+      let row = $(this);
+      let key = row.find('td')[0].text()
+      console.log(key);
+    })
     $('.show-content').append(sourceTable);
   });
 }
