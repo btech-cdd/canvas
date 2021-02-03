b44498c53cee4760c51fb25af610ed5c242dfe48
jhveem <joshua.hveem@gmail.com>
2021-02-03 09:30:45 -0700

    copying style from source table

--- custom_features/page_formatting/table_from_page.js
+++ custom_features/page_formatting/table_from_page.js
@@ -4,8 +4,10 @@ function dupStyle(to, from) {
 }
 let childTables = $('.btech-table-from-page');
 if (childTables.length > 0) {
+  console.log("CHILD EXISTS");
   let courseId = ENV.COURSE_ID;
   $.get('/api/v1/courses/'+courseId+'/pages/parts-list-master', function (data) {
+    console.log("SOURCE FOUND");
     let pBody = $('<div class=".page-body">' + data.body + '</div>');
     let sourceTable = pBody.find('.table-from-page-source');
     rowRef = {};
@@ -19,22 +21,37 @@ if (childTables.length > 0) {
     });
 
     childTables.each(function() {
-      let thead = sourceTable.find('thead').clone();
+      //Set up table
       let newTable = $('<table></table>');
-      // dupStyle(newTable, sourceTable);
+      dupStyle(newTable, sourceTable);
+      console.log("CREATE TABLE")
+      console.log(newTable);
+
+      //pull head directly from source
+      let thead = sourceTable.find('thead').clone();
       newTable.append(thead);
+      console.log(thead);
+
+      //Create a new body and duplicate style from source
       let tbody = $("<tbody></tbody>")
       // dupStyle(tbody, sourceTable.find('tbody'));
       newTable.append(tbody);
+      console.log(tbody);
+
+      //Go row by from and add in same order as what appears on page
       let childTable = $(this);
       let childRows = childTable.find('tbody tr');
       childRows.each(function() {
         let row = $(this);
         let key = $(row.find('td')[0]).text().toLowerCase();
+        console.log(key);
         if (key in rowRef) {
           tbody.append(rowRef[key].clone());
         }
-      })
+      });
+
+      //add the newly created table to the page
+      console.log("ADD TO PAGE");
       $('.show-content').append(newTable);
     })
   });
