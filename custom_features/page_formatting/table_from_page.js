3aad5be09211ae96eca8c016858ebf1219bf27c9
jhveem <joshua.hveem@gmail.com>
2021-02-03 09:00:24 -0700

    table from page

--- custom_features/page_formatting/table_from_page.js
+++ custom_features/page_formatting/table_from_page.js
@@ -1,6 +1,6 @@
 let childTables = $('.btech-table-from-page');
 if (childTables.length > 0) {
-  let courseId = ENV.courseId;
+  let courseId = ENV.COURSE_ID;
   $.get('/api/v1/courses/'+courseId+'/pages/parts-list-master', function (data) {
     let pBody = $('<div class=".page-body">' + data.body + '</div>');
     let sourceTable = pBody.find('.table-from-page-source');
