90b9227afee8556c73c30e281b3da95a2573ac57
jhveem <joshua.hveem@gmail.com>
2021-02-03 09:43:06 -0700

    table from page insert button

--- custom_features/editor_toolbar/tables.js
+++ custom_features/editor_toolbar/tables.js
@@ -63,37 +63,80 @@
     let selection = editor.selection;
     let bg = TOOLBAR.addBackground();
     bg.append(`
-    <div id='google-sheet-id-container' style='
-    width: 500px;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    position:fixed;
-    top: 50%;
-    z-index:1000;
-    transition: 0.5s;
-    background-color: #FFF;
-    border: 2px solid #888;
-    padding: 10px 20px;
-    color: #000;
-    border-radius: 5px;'>
-    Enter Google Sheet Id<br><input style='width: 100%;' type="text" id="google-sheet-id">
-    </div>
-    </div>`);
+      <div id='google-sheet-id-container' style='
+      width: 500px;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      position:fixed;
+      top: 50%;
+      z-index:1000;
+      transition: 0.5s;
+      background-color: #FFF;
+      border: 2px solid #888;
+      padding: 10px 20px;
+      color: #000;
+      border-radius: 5px;'>
+      Enter Google Sheet Id<br><input style='width: 100%;' type="text" id="google-sheet-id">
+      </div>
+      </div>`);
 
     $("#google-sheet-id").keypress(function (event) {
       var keycode = (event.keyCode ? event.keyCode : event.which);
       if (keycode == '13') {
         //*
         editor.execCommand("mceReplaceContent", false, `
-<table class="google-sheet-based sheet-` + $(this).val() + `">
-<tbody>
-<tr>
-<td>
-{$selection}
-</td>
-</tr>
-</tbody>
-</table>`);
+          <table class="google-sheet-based sheet-` + $(this).val() + `">
+          <tbody>
+          <tr>
+          <td>
+          {$selection}
+          </td>
+          </tr>
+          </tbody>
+          </table>`);
+        //*/
+        bg.remove();
+      }
+      event.stopPropagation();
+    });
+  }
+
+  async function tableFromPage() {
+    let editor = TOOLBAR.editor;
+    let selection = editor.selection;
+    let bg = TOOLBAR.addBackground();
+    bg.append(`
+      <div id='table-from-page-id-container' style='
+      width: 500px;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      position:fixed;
+      top: 50%;
+      z-index:1000;
+      transition: 0.5s;
+      background-color: #FFF;
+      border: 2px solid #888;
+      padding: 10px 20px;
+      color: #000;
+      border-radius: 5px;'>
+      Enter Canvas Page Id<br><input style='width: 100%;' type="text" id="table-from-page-id">
+      </div>
+      </div>`);
+
+    $("#table-from-page-id").keypress(function (event) {
+      var keycode = (event.keyCode ? event.keyCode : event.which);
+      if (keycode == '13') {
+        //*
+        editor.execCommand("mceReplaceContent", false, `
+          <table class="btech-table-from-page btech-table-from-page-source-` + $(this).val() + `">
+          <tbody>
+          <tr>
+          <td>
+          {$selection}
+          </td>
+          </tr>
+          </tbody>
+          </table>`);
         //*/
         bg.remove();
       }
@@ -103,6 +146,7 @@
 
   await TOOLBAR.checkReady();
   TOOLBAR.addButtonIcon("far fa-file-spreadsheet", "Insert a table which will be linked to a google sheet. You will need the google sheet id.", googleSheetsTable);
+  TOOLBAR.addButtonIcon("far fa-file-spreadsheet", "Insert a table which will be linked to a source table on a Canvas page. You will need the page id as it appears in the page URL.", tableFromPage);
   let select = TOOLBAR.addSelect("tables", "Convert tables to other display options.");
   for (let i = 0; i < tableOptions.length; i++) {
     let className = tableOptions[i];
