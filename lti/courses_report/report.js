e8bc7fa2fc58e1e190cdd218720aa09bcbf66881
jhveem <joshua.hveem@gmail.com>
2021-02-10 10:13:50 -0700

    adding courses report

---
+++ lti/courses_report/report.js
@@ -0,0 +1,70 @@
+(async function () {
+  async function delay(ms) {
+    // return await for better async stack trace support in case of errors.
+    return await new Promise(resolve => setTimeout(resolve, ms));
+  }
+  async function getElement(selectorText, iframe = "") {
+    let element;
+    if (iframe === "") {
+      element = $(selectorText);
+    } else {
+      element = $(iframe).contents().find(selectorText);
+    }
+    if (element.length > 0 && element.html().trim() !== "") {
+      return element;
+    } else {
+      await delay(250);
+      return getElement(selectorText, iframe);
+    }
+  }
+  this.APP = new Vue({
+    el: '#canvas-courses-report-vue',
+    mounted: async function () {
+      $('#canvas-courses-report-vue').css({
+        visibility: 'visible'
+      });
+      let app = this;
+      let dept = '' + CURRENT_DEPARTMENT;
+      app.loading = false;
+    },
+
+    computed: {
+    },
+
+    updated: function () {},
+
+    data: function () {
+      return {
+        loading: true,
+        colors: {
+          base: '#334',
+          black: '#000000',
+          white: '#ffffff',
+          red: 'rgb(217, 83, 79)',
+          orange: 'rgb(229, 128, 79)',
+          yellow: 'rgb(240, 173, 78)',
+          green: 'rgb(92, 184, 92)',
+          blue: 'rgb(91, 192, 222)',
+          gray: '#E0E0E0',
+          noProgress: '#E0E0E0',
+          complete: '#5BC0DE',
+          badDate: '#D9534F',
+          warningDate: '#F0AD4E',
+          goodDate: '#5CB85C',
+        },
+        scrollTop: 0, //used to save the scroll location of the main screen before going into the student report
+      }
+    },
+    methods: {
+      async loadJsonFile(name) {
+        let app = this;
+        let jsonUrl = 'https://jhveem.xyz/lti/report_data/' + name;
+        let jsonData = {};
+        await $.post(jsonUrl, function (data) {
+          jsonData = data;
+        });
+        app.json[name] = jsonData;
+      },
+    }
+  })
+})();
