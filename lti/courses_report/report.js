034adf5cc2b8ff444e7902db221aac2df2cb172a
jhveem <joshua.hveem@gmail.com>
2021-02-10 14:58:18 -0700

    course report

--- lti/courses_report/report.js
+++ lti/courses_report/report.js
@@ -93,12 +93,17 @@
 
       formatTime(time) {
         time = Math.round(time);
+        let hours = Math.floor(time / 3600)
+        time -= (hours * 3600);
         console.log(time);
         let minutes = Math.floor(time / 60);
         console.log(minutes);
         time -= (minutes * 60);
         let seconds = time;
-        return minutes + ":" + seconds; 
+        if (hours < 10) hours = "0" + hours;
+        if (minutes < 10) minutes = "0" + minutes;
+        if (seconds < 10) seconds = "0" + seconds;
+        return hours + ":"  + minutes + ":" + seconds; 
       },
 
       calcAverageQuizAlpha(quiz) {
