cf5d226d0c2ccded24b84660efecb3fa6cfee17c
jhveem <joshua.hveem@gmail.com>
2021-02-11 15:35:38 -0700

    update

--- lti/courses_report/report.js
+++ lti/courses_report/report.js
@@ -220,120 +220,46 @@
           "translate(" + margin.left + "," + margin.top + ")");
 
       let data = graph.getData();
-      console.log(data);
-    // Transpose the data into layers
-var dataset = d3.layout.stack()(['active', 'completed', 'dropped'].map(function(type) {
-  return data.map(function(d) {
-    return {x: parse(d.name), y: +d[type]};
-  });
-}));
+      stackdata = ["gold", "silver"].map(function (c) {
+        return data.map(function (d, i) {
+          return {
+            x: i,
+            y: d[c]
+          }
+        })
+      })
 
-
-// Set x, y and colors
-var x = d3.scale.ordinal()
-  .domain(dataset[0].map(function(d) { return d.x; }))
-  .rangeRoundBands([10, width-10], 0.02);
-
-var y = d3.scale.linear()
-  .domain([0, d3.max(dataset, function(d) {  return d3.max(d, function(d) { return d.y0 + d.y; });  })])
-  .range([height, 0]);
-
-var colors = ["b33040", "#d25c4d", "#f2b447", "#d9d574"];
-
-
-// Define and draw axes
-var yAxis = d3.svg.axis()
-  .scale(y)
-  .orient("left")
-  .ticks(5)
-  .tickSize(-width, 0, 0)
-  .tickFormat( function(d) { return d } );
-
-var xAxis = d3.svg.axis()
-  .scale(x)
-  .orient("bottom")
-  .tickFormat(d3.time.format("%Y"));
-
-svg.append("g")
-  .attr("class", "y axis")
-  .call(yAxis);
-
-svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")")
-  .call(xAxis);
-
-
-// Create groups for each series, rects for each segment 
-var groups = svg.selectAll("g.cost")
-  .data(dataset)
-  .enter().append("g")
-  .attr("class", "cost")
-  .style("fill", function(d, i) { return colors[i]; });
-
-var rect = groups.selectAll("rect")
-  .data(function(d) { return d; })
-  .enter()
-  .append("rect")
-  .attr("x", function(d) { return x(d.x); })
-  .attr("y", function(d) { return y(d.y0 + d.y); })
-  .attr("height", function(d) { return y(d.y0) - y(d.y0 + d.y); })
-  .attr("width", x.rangeBand())
-  .on("mouseover", function() { tooltip.style("display", null); })
-  .on("mouseout", function() { tooltip.style("display", "none"); })
-  .on("mousemove", function(d) {
-    var xPosition = d3.mouse(this)[0] - 15;
-    var yPosition = d3.mouse(this)[1] - 25;
-    tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
-    tooltip.select("text").text(d.y);
-  });
-
-
-// Draw legend
-var legend = svg.selectAll(".legend")
-  .data(colors)
-  .enter().append("g")
-  .attr("class", "legend")
-  .attr("transform", function(d, i) { return "translate(30," + i * 19 + ")"; });
- 
-legend.append("rect")
-  .attr("x", width - 18)
-  .attr("width", 18)
-  .attr("height", 18)
-  .style("fill", function(d, i) {return colors.slice().reverse()[i];});
- 
-legend.append("text")
-  .attr("x", width + 5)
-  .attr("y", 9)
-  .attr("dy", ".35em")
-  .style("text-anchor", "start")
-  .text(function(d, i) { 
-    switch (i) {
-      case 0: return "Anjou pears";
-      case 1: return "Naval oranges";
-      case 2: return "McIntosh apples";
-      case 3: return "Red Delicious apples";
-    }
-  });
-
-
-// Prep the tooltip bits, initial display is hidden
-var tooltip = svg.append("g")
-  .attr("class", "tooltip")
-  .style("display", "none");
-    
-tooltip.append("rect")
-  .attr("width", 30)
-  .attr("height", 20)
-  .attr("fill", "white")
-  .style("opacity", 0.5);
-
-tooltip.append("text")
-  .attr("x", 15)
-  .attr("dy", "1.2em")
-  .style("text-anchor", "middle")
-  .attr("font-size", "12px")
-  .attr("font-weight", "bold");
+      // Set up stack method
+      var stack = d3.layout.stack()
+      // Data, stacked
+      stack(stackdata)
+      // gold silver colors
+      var colors = ["gold", "silver"]
+      // Add a group for each row of data
+      var groups = svg.selectAll("g")
+        .data(stackdata)
+        .enter()
+        .append("g")
+        .style("fill", function (d, i) {
+          return colors[i]
+        })
+      // Add a rect for each data value
+      var rects = groups.selectAll("rect")
+        .data(function (d) {
+          return d
+        })
+        .enter()
+        .append("rect")
+        .attr("x", function (d, i) {
+          return i * 28
+        })
+        .attr("y", function (d) {
+          return d.y0
+        })
+        .attr("height", function (d) {
+          return d.y
+        })
+        .attr("width", 24)
     }
     async _initOld(app = APP, graphElId = 'btech-department-report-student-submissions-graph', w = 800, h = 240) {
       this.app = app;
