5003b540c3b5b631e42c2ef29120ad82ae9d9556
jhveem <joshua.hveem@gmail.com>
2021-02-11 15:06:24 -0700

    update

--- lti/courses_report/report.js
+++ lti/courses_report/report.js
@@ -223,64 +223,53 @@
 
       let data = graph.getData();
       console.log(data);
-      var subgroups = data.columns.slice(1)
+      // List of subgroups = header of the csv files = soil condition here
+  var subgroups = data.columns.slice(1)
 
-      // List of groups = species here = value of the first column called group -> I show them on the X axis
-      var groups = d3.map(data, function (d) {
-        return (d.name)
-      }).keys()
-      console.log(groups);
-      // Add X axis
-      var x = d3.scaleBand()
-        .domain(groups)
-        .range([0, width])
-        .padding([0.2])
-      svg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x).tickSizeOuter(0));
+  // List of groups = species here = value of the first column called group -> I show them on the X axis
+  var groups = d3.map(data, function(d){return(d.group)}).keys()
 
-      // Add Y axis
-      var y = d3.scaleLinear()
-        .domain([0, 60])
-        .range([height, 0]);
-      svg.append("g")
-        .call(d3.axisLeft(y));
+  // Add X axis
+  var x = d3.scaleBand()
+      .domain(groups)
+      .range([0, width])
+      .padding([0.2])
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSizeOuter(0));
 
-      // color palette = one color per subgroup
-      var color = d3.scaleOrdinal()
-        .domain(subgroups)
-        .range(['#e41a1c', '#377eb8', '#4daf4a'])
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 60])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
 
-      //stack the data? --> stack per subgroup
-      var stackedData = d3.stack()
-        .keys(subgroups)
-        (data)
+  // color palette = one color per subgroup
+  var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#e41a1c','#377eb8','#4daf4a'])
 
-      // Show the bars
-      svg.append("g")
-        .selectAll("g")
-        // Enter in the stack data = loop key per key = group per group
-        .data(stackedData)
-        .enter().append("g")
-        .attr("fill", function (d) {
-          return color(d.key);
-        })
-        .selectAll("rect")
-        // enter a second time = loop subgroup per subgroup to add all rectangles
-        .data(function (d) {
-          return d;
-        })
-        .enter().append("rect")
-        .attr("x", function (d) {
-          return x(d.data.group);
-        })
-        .attr("y", function (d) {
-          return y(d[1]);
-        })
-        .attr("height", function (d) {
-          return y(d[0]) - y(d[1]);
-        })
-        .attr("width", x.bandwidth())
+  //stack the data? --> stack per subgroup
+  var stackedData = d3.stack()
+    .keys(subgroups)
+    (data)
+
+  // Show the bars
+  svg.append("g")
+    .selectAll("g")
+    // Enter in the stack data = loop key per key = group per group
+    .data(stackedData)
+    .enter().append("g")
+      .attr("fill", function(d) { return color(d.key); })
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("x", function(d) { return x(d.data.group); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width",x.bandwidth())
     }
     async _initOld(app = APP, graphElId = 'btech-department-report-student-submissions-graph', w = 800, h = 240) {
       this.app = app;
