139f939afb92c48b731add1594757ef73d63fcd7
jhveem <joshua.hveem@gmail.com>
2021-02-24 12:17:15 -0700

    show hours

---
+++ custom_features/modules/show_hours.js
@@ -0,0 +1,74 @@
+(async function() {
+	let checkbox = $('<input type="checkbox"><label>Show Hours</label>');
+	checkbox.change(function() {
+		let checked = $(this).is(':checked')
+		console.log(checked);
+		if (checked) addHours();
+		else clearHours();
+	});
+	$("#external-tool-mount-point").append(checkbox);
+	let course_id = 502362;
+	async function clearHours() {
+		$('.btech-sum-hours').each(function() {
+			$(this).remove();
+		});
+	}
+	async function addHours() {
+		//get course hours
+		let hours = 0;
+		await $.get("/api/v1/courses/"+course_id, function(data) {
+			console.log(data);
+			let regex = /([A-Z]{4} [0-9]{4}) .*?([0-9]{4})(CS|HS|ST)/
+			let sis_id = data.sis_course_id;
+			let match = sis_id.match(regex);
+			console.log(match);
+			if (match.length > 0) {
+				let course_code = match[1];
+				let year = match[2];
+				hours = COURSE_HOURS[year][course_code];
+				console.log(course_code);
+				console.log(hours);
+			}
+		});
+		
+		let assignments = {};
+		let groups = {};
+		await $.get("/api/v1/courses/"+course_id+"/assignment_groups?include[]=assignments", function(data) {
+			for (let i = 0; i < data.length; i++) {
+				let group = data[i];
+				if (group.group_weight > 0) {
+					let sum = 0;
+					for (let j = 0; j < group.assignments.length; j++) {
+						let assignment = group.assignments[j];
+						sum += assignment.points_possible;
+						assignment.group_name = group.name;
+						assignments[assignment.name] = assignment;
+					}
+					groups[group.name] = {
+						sum: sum, 
+						weight: group.group_weight
+					};
+				}
+			}
+		});
+		console.log(assignments);
+		let sumPointsToHours = 0;
+		await $.get("/api/v1/courses/"+course_id+"/modules?include[]=items", function(data) {
+			//console.log(data);
+			for (let i = 0; i < data.length; i++) {
+				let module = data[i];
+				for (let j = 0; j < module.items.length; j++) {
+					let item = module.items[j];
+					if (item.title in assignments) {
+						let assignment = assignments[item.title];
+						let pointsToHours = (assignment.points_possible / groups[assignment.group_name].sum) * groups[assignment.group_name].weight * .01 * hours;
+						sumPointsToHours += pointsToHours; 
+						let li = $('#context_module_item_' + item.id);
+						li.css({'position': 'relative'});
+						li.prepend('<div class="btech-sum-hours" style="position: absolute; left: -4rem; padding: 4px; border-radius: 10px; font-size: .75rem; background-color: rgb(91, 192, 222); color: #fff;">'+(Math.round(sumPointsToHours * 10) / 10)+' HRS</div>');
+					}
+				}
+			}
+		});
+	}
+})();
