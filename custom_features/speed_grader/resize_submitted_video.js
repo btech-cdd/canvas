e5ec29b564959056a66d6d0264070bb129682692
jhveem <JoshuaH@batc.local>
2021-04-13 08:17:44 -0600

    try on iframe then try on current focus

--- custom_features/speed_grader/resize_submitted_video.js
+++ custom_features/speed_grader/resize_submitted_video.js
@@ -1,34 +1,35 @@
 //observes the side of the page where submission details are displayed and adjusts submission videos to fill the width of that space when the page resizes.
 //does not resize the video as soon as it appears. This could be the next step.
 //The final version should allow the instructor to just resize the video to whatever they want. Even better would be if it saved settings using Canvas's custom data to remember what size video the instructor prefers.
-function resizeVideo() {
-    let container = $("#speedgrader_iframe").contents().find("#media_recording_box");
-        container.width("100%");
-        let videoDiv = $("#speedgrader_iframe").contents().find(".mejs-video");
-        videoDiv.width("100%");
-        let width = videoDiv.width();
-        videoDiv.height(videoDiv.width() * .66);
-        let videoEl = $("#speedgrader_iframe").contents().find("video");
-        videoEl.width(width);
-        videoEl.height(videoEl.width() * .66);
-        videoEl.css({
-            "width": videoEl.width(),
-            "height": videoEl.height()
+function resizeVideo(frame) {
+    let container = frame.find("#media_recording_box");
+    container.width("100%");
+    let videoDiv = frame.find(".mejs-video");
+    videoDiv.width("100%");
+    let width = videoDiv.width();
+    videoDiv.height(videoDiv.width() * .66);
+    let videoEl = frame.find("video");
+    videoEl.width(width);
+    videoEl.height(videoEl.width() * .66);
+    videoEl.css({
+        "width": videoEl.width(),
+        "height": videoEl.height()
+    });
+    let layers = videoDiv.find(".mejs-layer");
+    layers.each(function() {
+        let el = $(this);
+        el.width(width);
+        el.height($(this).width() * .66);
+        el.css({
+            "width": el.width(),
+            "height": el.height()
         });
-        let layers = videoDiv.find(".mejs-layer");
-        layers.each(function() {
-            let el = $(this);
-            el.width(width);
-            el.height($(this).width() * .66);
-            el.css({
-                "width": el.width(),
-                "height": el.height()
-            });
-        });
-    }
-    let left = $("#left_side")[0];
-    let resizeObserver = new ResizeObserver(() => { 
-        resizeVideo();
-    }); 
-      
-    resizeObserver.observe(left);
+    });
+}
+let left = $("#left_side")[0];
+let resizeObserver = new ResizeObserver(() => { 
+    resizeVideo($("#speedgrader_iframe").contents());
+    resizeVideo($("body"));
+}); 
+    
+resizeObserver.observe(left);
