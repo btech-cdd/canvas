90677aeb8836c7d8100de987043617a822ce0c59
joshua.hveem@gmail.com <JoshuaH@batc.local>
2021-05-21 15:15:29 -0600

    default show ungraded assignment

--- custom_features/grades_page/default_include_ungraded_assignments.js
+++ custom_features/grades_page/default_include_ungraded_assignments.js
@@ -1,7 +1,15 @@
 let gradesEl = $(".final_grade .grade");
-console.log(gradesEl.text());
-let currentGrade = gradesEl.text().match(/([0-9]+(\.[0-9]+){0,1}%)/)[1];
-console.log(currentGrade);
+let currentGrade = 0;
+try {
+  currentGrade = parseFloat(gradesEl.text().match(/([0-9]+(\.[0-9]+){0,1})%/)[1]);
+} catch(e) {
+  console.log(e);
+}
 $("#only_consider_graded_assignments_wrapper").find('label').click();
-let finalGrade = gradesEl.text().match(/([0-9]+(\.[0-9]+){0,1}%)/)[1];
-console.log(finalGrade);
+let finalGrade = 0;
+try {
+  finalGrade = parseFloat(gradesEl.text().match(/([0-9]+(\.[0-9]+){0,1})%/)[1]);
+} catch(e) {
+  console.log(e);
+}
+if (currentGrade > 0) console.log(finalGrade / currentGrade);
