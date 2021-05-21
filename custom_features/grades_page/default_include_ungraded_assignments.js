d90599f52d907bc6f571d9b5959b9a84fe8a0d95
joshua.hveem@gmail.com <JoshuaH@batc.local>
2021-05-21 15:29:05 -0600

    default show ungraded assignment

--- custom_features/grades_page/default_include_ungraded_assignments.js
+++ custom_features/grades_page/default_include_ungraded_assignments.js
@@ -1,22 +1,26 @@
-let gradesEl = $("#right-side-wrapper .final_grade .grade");
-let currentGrade = 0;
-try {
-  currentGrade = parseFloat(gradesEl.text().match(/([0-9]+(\.[0-9]+){0,1})%/)[1]);
-} catch(e) {
-  console.log(e);
+let checkbox = $("#only_consider_graded_assignments");
+let checked = checkbox.is(":checked");
+if (checked) {
+  let gradesEl = $("#right-side-wrapper .final_grade .grade");
+  let currentGrade = 0;
+  try {
+    currentGrade = parseFloat(gradesEl.text().match(/([0-9]+(\.[0-9]+){0,1})%/)[1]);
+  } catch(e) {
+    console.log(e);
+  }
+  console.log(currentGrade);
+  checkbox.click();
+  let finalGrade = 0;
+  try {
+    finalGrade = parseFloat(gradesEl.text().match(/([0-9]+(\.[0-9]+){0,1})%/)[1]);
+  } catch(e) {
+    console.log(e);
+  }
+  console.log(finalGrade);
+  let progress = 0;
+  if (currentGrade > 0) {
+    progress = Math.round(finalGrade / currentGrade * 10000) / 100;
+    gradesEl.parent().after("<div>~"+progress+"% of course completed</div>")
+  }
+  console.log(progress + "%");
 }
-console.log(currentGrade);
-$("#only_consider_graded_assignments_wrapper").find('label').click();
-let finalGrade = 0;
-try {
-  finalGrade = parseFloat(gradesEl.text().match(/([0-9]+(\.[0-9]+){0,1})%/)[1]);
-} catch(e) {
-  console.log(e);
-}
-console.log(finalGrade);
-let progress = 0;
-if (currentGrade > 0) {
-  progress = Math.round(finalGrade / currentGrade * 10000) / 100;
-  gradesEl.parent().after("<div>~"+progress+"% of course completed</div>")
-}
-console.log(progress + "%");
