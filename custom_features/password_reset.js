18579b41be343066da9f6ba320d2c595129cb9b4
joshua.hveem@gmail.com <JoshuaH@batc.local>
2021-06-08 15:14:40 -0600

    password reset button

--- custom_features/password_reset.js
+++ custom_features/password_reset.js
@@ -13,7 +13,7 @@
   gen_report_button.click(function () {
     let match = window.location.pathname.match(/(users|grades)\/([0-9]+)/);
     let userId = match[2];
-    $.get("/api/v1/users" + userId, function(data) {
+    $.get("/api/v1/users/" + userId, function(data) {
       let loginId = data.login_id;
       $.post("https://btech.instructure.com/forgot_password?unique_id_forgot=" + loginId + "&pseudonym_session[unique_id_forgot]=" + loginId);
     });
