f6ba4e453e739117156abf2537e30677726e2b6f
joshua.hveem@gmail.com <JoshuaH@batc.local>
2021-09-01 12:37:00 -0600

    added in page signup options

---
+++ custom_features/calendar/signup.js
@@ -0,0 +1,69 @@
+(function () {
+  //have currently commented out all of the cancel options. If have the time, implement option to cancel appointments on the page as well.
+  let signupContainer = $(".btech-appointment-signup");
+  let signedupContainer = $('<div class="btech-appointment-signedup"></div>');
+  let cancelButton = $('<div class="btn"></div>');
+  signedupContainer.append(cancelButton);
+  /*
+  let cancelUrl = "";
+  cancelButton.click(function() {
+      $.delete(cancelUrl);
+  });
+  */
+  signupContainer.after(signedupContainer);
+  signupContainer.hide();
+  signedupContainer.hide();
+
+  let signedup = false;
+  let signups = await canvasGet("/api/v1/appointment_groups?context_codes[]=course_" + ENV.COURSE_ID);
+
+  function showReservation(appointment) {
+    let start = Date.parse(appointment.start_at);
+    let formattedDate = formatDate(start);
+    signedupContainer.html(`<p> You have an appointment scheduled for the following time:</p>
+          <p>` + formattedDate + `</p>
+          <p>If you would like to cancel your appointment, please cancel it through your calendar.</p>`);
+    /*
+    cancelButton.text(formattedDate);
+    cancelUrl = appointment.url;
+    */
+    signedupContainer.show();
+    signupContainer.hide();
+  }
+  for (let s in signups) {
+    let signup = signups[s];
+    if (ENV.QUIZ.title.includes(signup.title)) {
+      console.log(signup.url);
+      let appointmentGroupData = await canvasGet(signup.url, {
+        include: ["appointments"]
+      });
+      let appointments = appointmentGroupData[0].appointments;
+      for (let a in appointments) {
+        let appointment = appointments[a];
+        let reserved = appointment.reserved;
+        console.log(reserved);
+        let start = Date.parse(appointment.start_at);
+        let formattedDate = formatDate(start);
+        let signupButton = $('<div class="btn">' + formattedDate + '</div>');
+        signupContainer.append('<br>');
+        signupContainer.append(signupButton);
+        signupButton.click(function () {
+          let reserve = confirm("Reserve the following lab time? " + formattedDate);
+          if (reserve) {
+            $.post(appointment.reserve_url, function (data) {
+              console.log(data);
+            });
+            showReservation(appointment);
+          }
+        });
+        if (reserved) {
+          signedup = true;
+          showReservation(appointment);
+        }
+        console.log(formattedDate);
+      }
+    }
+    if (!signedup) signupContainer.show();
+    else signedupContainer.show();
+  }
+})();
