144ec48e6a1607da7471a571918ac77cf42311b7
Joshua Hveem <JoshuaH@batc.local>
2021-09-17 12:04:03 -0600

    update banner

--- custom_features/welcome_banner.js
+++ custom_features/welcome_banner.js
@@ -1,10 +1,8 @@
 let main = $("#dashboard");
 main.prepend(`
 <div class="ic-notification">
-  <div class="ic-notification__content">
-    <a href="/courses/480103" target="_blank">
-      <img src="` + SOURCE_URL + `/media/small_orientation_banner.png">
-    </a>
-  </div>
+  <a href="/courses/480103" target="_blank">
+    <img src="` + SOURCE_URL + `/media/small_orientation_banner.png">
+  </a>
 </div>
 `);
