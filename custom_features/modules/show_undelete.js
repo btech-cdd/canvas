72578808e7bfa73063ffa04f21df61019110cfc4
Joshua Hveem <jhveem@btech.edu>
2022-05-31 13:49:35 -0600

    data analytics

--- custom_features/modules/show_undelete.js
+++ custom_features/modules/show_undelete.js
@@ -1,1 +1,1 @@
-$(".header-bar-right__buttons").prepend(`<a class="btn module_progressions_link" href="/courses/${ENV.COURSE_ID}/undelete">Undelete</a>`)
+$(".header-bar-right__buttons").prepend(`<a class="btn module_progressions_link" href="/courses/${ENV.COURSE_ID}/undelete">Undelete</a>`);
