d4188fe47de6772f5f5d9b59b5be2ec718aa09fa
Joshua Hveem <jhveem@btech.edu>
2022-06-03 09:26:45 -0600

    filter icon

--- custom_features/reports/grades_page/template.vue
+++ custom_features/reports/grades_page/template.vue
@@ -5,9 +5,6 @@
       <div class='btech-modal-content-inner'>
         <span class='btech-close' v-on:click='close()'>&times;</span>
         <h3 style='text-align: center;'>Report</h3>
-        <h5 style='text-align: center;'>Click on column headers to sort by that column.</h5>
-        <h5 style='text-align: center;'>Hover over column headers for a description of the information displayed in that
-          column.</h5>
         <div
           style="display: inline-block;"
         >
