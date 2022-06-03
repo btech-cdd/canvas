6c1e4fbdf17fe4e25a2e603afa2033af56abe393
Joshua Hveem <jhveem@btech.edu>
2022-06-03 08:20:17 -0600

    allow for different progress calc methods

--- custom_features/reports/grades_page/template.vue
+++ custom_features/reports/grades_page/template.vue
@@ -37,8 +37,8 @@
           <div 
             style="
               padding: .25rem .5rem;
-              display: flex;
-              flex: 20% 20% 4.5rem 4.5rem 10rem 7rem 5rem;
+              display: grid;
+              grid-template-columns: 20% 20% 4.5rem 4.5rem 10rem 7rem 5rem;
               align-items: center;
               justify-content: center;
               font-size: 0.75rem;
