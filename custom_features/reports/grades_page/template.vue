cb37d121416ebe0ef13651915f8b4bc4b9ed7814
Joshua Hveem <jhveem@btech.edu>
2022-06-03 09:24:46 -0600

    filter icon

--- custom_features/reports/grades_page/template.vue
+++ custom_features/reports/grades_page/template.vue
@@ -46,10 +46,19 @@
               @click="sortColumn(column.name)"
             >
               <span><b>{{column.name}}</b></span>
-              <svg style="width: .75rem; height: .75rem;" x="0px" y="0px"
-                viewBox="0 0 490 490" style="enable-background:new 0 0 490 490;" xml:space="preserve">
+              <svg 
+                style="width: .75rem; height: .75rem;" 
+                x="0px" 
+                y="0px"
+                viewBox="0 0 490 490" 
+                style="enable-background:new 0 0 490 490;" 
+                xml:space="preserve">
               <g>
-                <polygon points="85.877,154.014 85.877,428.309 131.706,428.309 131.706,154.014 180.497,221.213 217.584,194.27 108.792,44.46 
+                <polygon 
+                  :style="{
+                    'fill': column.sort > 0 ? '#000000' : '#E0E0E0'
+                  }"
+                  points="85.877,154.014 85.877,428.309 131.706,428.309 131.706,154.014 180.497,221.213 217.584,194.27 108.792,44.46 
                   0,194.27 37.087,221.213 	"/>
                 <polygon points="404.13,335.988 404.13,61.691 358.301,61.691 358.301,335.99 309.503,268.787 272.416,295.73 381.216,445.54 
                   490,295.715 452.913,268.802 	"/>
