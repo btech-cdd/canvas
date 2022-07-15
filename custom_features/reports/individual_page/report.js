40d97dc873cd212bd3f593b366848dc4c08ad52d
Joshua Hveem <jhveem@btech.edu>
2022-07-15 10:22:50 -0600

    color if too low hours

--- custom_features/reports/individual_page/report.js
+++ custom_features/reports/individual_page/report.js
@@ -350,7 +350,7 @@
   
   try {
     let user = await bridgetoolsReq(`https://reports.bridgetools.dev/api/students/${ENV.current_user_id}`);
-    if (user?.enrollment_type == 'HS') {
+    if (user?.enrollment_type == 'HS' && false) {
       $("#content").prepend(`
         <div style="background-color: white; position:relative; left: 0; bottom: 0;" class="ic-notification ic-notification--danger">
           <div class="ic-notification__icon" role="presentation">
