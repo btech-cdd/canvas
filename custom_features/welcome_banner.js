b06bae74ed70c5056fb76e7a7d84c9c24f5a504f
Joshua Hveem <jhveem@btech.edu>
2022-08-08 14:53:10 -0600

    add settings to courses

--- custom_features/welcome_banner.js
+++ custom_features/welcome_banner.js
@@ -35,5 +35,6 @@ function showSlide() {
   if (slideIndex > x.length) {slideIndex = 1}
   if (slideIndex < 0) {x.length - 1}
   x[slideIndex-1].style.display = "block";
+  if (IS_CDD)
   setTimeout(() => { slideIndex += 1; showSlide(); }, 5000); // Change image every 2 seconds
 }
