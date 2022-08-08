34714f558937665887d47887ccb6a7a02f78660e
Joshua Hveem <jhveem@btech.edu>
2022-08-08 14:15:40 -0600

    add settings to courses

--- custom_features/welcome_banner.js
+++ custom_features/welcome_banner.js
@@ -1,6 +1,31 @@
 let main = $("#content-wrapper");
 main.prepend(`
-  <a href="/courses/480103" target="_blank">
-    <img src="` + SOURCE_URL + `/media/small_orientation_banner.png">
-  </a>
+  <div
+    class="btech-banner-slides-container"
+  >
+    <div
+      class="btech-banner-slide"
+    >
+    <a href="/courses/480103" target="_blank">
+      <img src="` + SOURCE_URL + `/media/small_orientation_banner.png">
+    </a>
+    </div>
+  </div>
 `);
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("btech-banner-slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
