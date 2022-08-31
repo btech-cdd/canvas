35a1a6ba355c9eb686e570fb6c0657538ec56135
Joshua Hveem <jhveem@btech.edu>
2022-08-31 13:55:02 -0600

    login page

--- custom_canvas.js
+++ custom_canvas.js
@@ -253,7 +253,7 @@ if (window.self === window.top) { //Make sure this is only run on main page, and
           });
         }
 
-        if (ENV?.current_user_roles.includes('root_admin')) {
+        if (ENV?.current_user_roles?.includes('root_admin')) {
           feature("remove_former_employees", {}, /^\/accounts\/3\/users\/[0-9]+/)
         }
 
