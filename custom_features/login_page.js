8f61dbb7e8144a39c21741f7e1ee3f341103af23
Joshua Hveem <jhveem@btech.edu>
2022-08-31 13:51:35 -0600

    submitting

---
+++ custom_features/login_page.js
@@ -0,0 +1,3 @@
+$("forgot_password_instructions").after(`
+<p>If you don't receive a password reset email within 30 minutes, reach out to <a href="https://cases.canvaslms.com/liveagentchat?sfid=001A000000jePmGIAU">Canvas support</a>.</p>
+`)
