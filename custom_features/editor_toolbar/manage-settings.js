66759014c8145887f1f0366119f7de3cc8abe187
Joshua Hveem <jhveem@btech.edu>
2022-12-09 13:42:45 -0700

    manage toolbar settings

--- custom_features/editor_toolbar/manage-settings.js
+++ custom_features/editor_toolbar/manage-settings.js
@@ -9,7 +9,7 @@
       <div
         v-for="val, setting in settings.misc"
       >
-        <span>{{setting}}</span><input @click="updateSettings" type="checkbox" :id="'misc-' + setting" v-model="val">
+        <span>{{setting}}</span><input @click="updateSettings" type="checkbox" :id="'misc-' + setting" v-model="settings.misc[val]">
       </div>
       <p>hello world</p>
     </div>
