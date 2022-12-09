022c5e1714c4de06c0e42c60e8b9b7d646e873ae
Joshua Hveem <jhveem@btech.edu>
2022-12-09 13:49:29 -0700

    manage toolbar settings

--- custom_features/editor_toolbar/manage-settings.js
+++ custom_features/editor_toolbar/manage-settings.js
@@ -56,7 +56,7 @@
       async parseCanvasData(settings) {
         console.log(settings);
         console.log(typeof settings);
-        if (settings != undefined && typeof settings !== "object") {
+        if ((settings !== undefined) && (typeof settings !== "object")) {
           for (let category in settings) {
             let val = settings[category]
             if (typeof val != "string") {
