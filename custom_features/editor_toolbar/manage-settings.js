7c619c809b50c29d4de75f247f51875d8e0ff3dc
Joshua Hveem <jhveem@btech.edu>
2022-12-09 12:44:58 -0700

    manage toolbar settings

--- custom_features/editor_toolbar/manage-settings.js
+++ custom_features/editor_toolbar/manage-settings.js
@@ -1,16 +1,16 @@
 (async function() {
   console.log("TOOLBAR");
   $("#content").empty();
-  expandButton.click(function() {
-    let maxWidth = getCSSVar("--btech-max-width");
-    if (maxWidth == "auto") {
-      $(expandButton.find("svg")).attr("fill", "#AAA");
-      $.put(`/api/v1/users/self/custom_data?ns=com.btech&data[page_width]=default`);
-      setCSSVar("--btech-max-width", DEFAULT_MAX_WIDTH);
-    } else {
-      $(expandButton.find("svg")).attr("fill", "#000000");
-      $.put(`/api/v1/users/self/custom_data?ns=com.btech&data[page_width]=auto`);
-      setCSSVar("--btech-max-width", "auto");
-    }
-  })
+  // expandButton.click(function() {
+  //   let maxWidth = getCSSVar("--btech-max-width");
+  //   if (maxWidth == "auto") {
+  //     $(expandButton.find("svg")).attr("fill", "#AAA");
+  //     $.put(`/api/v1/users/self/custom_data?ns=com.btech&data[page_width]=default`);
+  //     setCSSVar("--btech-max-width", DEFAULT_MAX_WIDTH);
+  //   } else {
+  //     $(expandButton.find("svg")).attr("fill", "#000000");
+  //     $.put(`/api/v1/users/self/custom_data?ns=com.btech&data[page_width]=auto`);
+  //     setCSSVar("--btech-max-width", "auto");
+  //   }
+  // })
 })();
