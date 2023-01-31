fb24e3c7bdb0dea8fa2158c6253e692ab54a879d
Joshua Hveem <jhveem@btech.edu>
2023-01-31 08:48:46 -0700

    cling

--- custom_features/img-zoom.js
+++ custom_features/img-zoom.js
@@ -1,28 +1,30 @@
 console.log("IMG ZOOM");
-$("span").each(function() {
-  let el = $(this);
-  let bgimg = el.css("background-image");
-  if (bgimg != "none") {
-    console.log(bgimg);
-      let hoverimg = $(el.clone());
-      hoverimg.css({
-          width: "256px",
-          height: "256px",
-          position: "absolute",
-          "margin-left": "-96px",
-          "margin-top": "-96px",
-          "pointer-events": "none",
-          "z-index": 10000000
-      });
-      el.before(hoverimg);
-      hoverimg.hide();
-      el.hover(
-          function() {
-              hoverimg.show();
-          },
-          function() {
-              hoverimg.hide();
-          }
-      );
-  }
-});
+(function() {
+  $("span").each(function() {
+    let el = $(this);
+    let bgimg = el.css("background-image");
+    if (bgimg != "none") {
+      console.log(bgimg);
+        let hoverimg = $(el.clone());
+        hoverimg.css({
+            width: "256px",
+            height: "256px",
+            position: "absolute",
+            "margin-left": "-96px",
+            "margin-top": "-96px",
+            "pointer-events": "none",
+            "z-index": 10000000
+        });
+        el.before(hoverimg);
+        hoverimg.hide();
+        el.hover(
+            function() {
+                hoverimg.show();
+            },
+            function() {
+                hoverimg.hide();
+            }
+        );
+    }
+  });
+})();
