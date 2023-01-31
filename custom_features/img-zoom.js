d77b28fef069079a5030726c81b25ba633190909
Joshua Hveem <jhveem@btech.edu>
2023-01-31 08:43:16 -0700

    cling

---
+++ custom_features/img-zoom.js
@@ -0,0 +1,26 @@
+$("span").each(function() {
+  let el = $(this);
+  let bgimg = el.css("background-image");
+  if (bgimg != "none") {
+      let hoverimg = $(el.clone());
+      hoverimg.css({
+          width: "256px",
+          height: "256px",
+          position: "absolute",
+          "margin-left": "-96px",
+          "margin-top": "-96px",
+          "pointer-events": "none",
+          "z-index": 10000000
+      });
+      el.before(hoverimg);
+      hoverimg.hide();
+      el.hover(
+          function() {
+              hoverimg.show();
+          },
+          function() {
+              hoverimg.hide();
+          }
+      );
+  }
+});
