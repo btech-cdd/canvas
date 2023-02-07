26491ea72660606f482249608cba48f67efaf0c6
Joshua Hveem <jhveem@btech.edu>
2023-02-07 09:55:38 -0700

    show 2020 results?

--- custom_features/grades_page/highlighted_grades_page_items_dental.js
+++ custom_features/grades_page/highlighted_grades_page_items_dental.js
@@ -5,6 +5,33 @@ if (/^\/courses\/[0-9]+\/grades\/[0-9]+$/.test(window.location.pathname)) {
       <span class="unread_dot grade_dot" style="background-color: ${color};">&nbsp;</span>
     `);
   }
+  let highlightColor = "#FFC";
+  let submissions = {};
+  let submissionsData = ENV.submissions;
+  for (let s in submissionsData) {
+    let submission = submissionsData[s];
+    submissions[submission.assignment_id] = submission;
+  }
+  let groups = ENV.assignment_groups;
+  for (let g in groups) {
+    let group = groups[g];
+    let assignments = group.assignments;
+    for (let a in assignments) {
+      let assignment = assignments[a];
+      if (!!submissions?.[assignment.id]) {
+        let submission = submissions[assignment.id];
+        let score = submission.score;
+        let possible = assignment.points_possible;
+        let perc= score / possible;
+        let el =  $(`#submission_${assignment.id}`);
+        if (perc < .8) {
+          let context = el.find("div.context").text();
+          addDot(el, "#FC0");
+        }
+      }
+    }
+  }
+  /*
   $("tr.student_assignment").each(function() {
     let el = $(this);
     let context = $(this).find("div.context").text();
@@ -34,17 +61,7 @@ if (/^\/courses\/[0-9]+\/grades\/[0-9]+$/.test(window.location.pathname)) {
         let criteria = $(table).find("tr.rubric-criterion");
         let completed = true;
         criteria.each(function() {
-            //OPTION 1, JUST CHECK THE Time 1.0 CRITERION
-            /*
-            let isCompletedCriterion = $(this).find("th.description-header").text().includes("Time 1.0");
-            if (isCompletedCriterion) {
-                let ratings = $(this).find("div.rating-tier-list div.rating-tier");
-                completed = $(ratings[0]).hasClass("selected");
-            }
-            //*/
-
-            //OPTION 2, CHECK EVERY CRITERIA EXCEPT FOR ATTEMPTS
-            //*
+            //CHECK EVERY CRITERIA EXCEPT FOR ATTEMPTS
             let isAttemptsCriterion = $(this).find("th.description-header").text().includes("Attempts");
             //CHECK ALL CRITERIA EXCEPT ATTEMPTS
             if (!isAttemptsCriterion) {
@@ -54,12 +71,12 @@ if (/^\/courses\/[0-9]+\/grades\/[0-9]+$/.test(window.location.pathname)) {
                     completed = false;
                 }
             }
-            //*/
         });
         if (completed === false) {
           addDot(el, "#FC0");
         }
       }
     }
+    */
   });
 }
