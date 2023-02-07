a4e917607b092e26dedc5b305d60b49ab83c7f1f
Joshua Hveem <jhveem@btech.edu>
2023-02-07 10:35:01 -0700

    changing grade highliter

--- custom_features/grades_page/highlighted_grades_page_items_dental.js
+++ custom_features/grades_page/highlighted_grades_page_items_dental.js
@@ -1,44 +1,57 @@
 //THIS HAS VERY MUCH BEEN TAILORED TO DENTAL. IT WILL NEED TO BE REWORKED TO BE FLEXIBLE ACROSS DEPARTMENTS
 if (/^\/courses\/[0-9]+\/grades\/[0-9]+$/.test(window.location.pathname)) {
+
+  //adds a colored dot next to the score on the grades page
   function addDot(el, color="#FC0") {
     $(el.find(".assignment_score .score_holder")).append(`
       <span class="unread_dot grade_dot" style="background-color: ${color};">&nbsp;</span>
     `);
   }
+
+  //create lookup dict of submissions
   let submissions = {};
   let submissionsData = ENV.submissions;
   for (let s in submissionsData) {
     let submission = submissionsData[s];
     submissions[submission.assignment_id] = submission;
-    if (submission.assignment_id == 5353425) {
-    }
   }
+
+  //iterate over assignments group by group
   let groups = ENV.assignment_groups;
   for (let g in groups) {
     let group = groups[g];
     let assignments = group.assignments;
     for (let a in assignments) {
+
+      //get assignment sumbission details
       let assignment = assignments[a];
       if (!!submissions?.[assignment.id]) {
         let el =  $(`#submission_${assignment.id}`);
         let submission = submissions[assignment.id];
         let score = submission.score;
+
+        //If needs grading, mark in red for instructor
         if (submission.workflow_state == 'submitted' && score == null) {
           addDot(el, "#C00");
           continue;
         }
+
+        //if graded, check if it meets score requirements
         let possible = assignment.points_possible;
         let perc = score / possible;
         if (score != null) {
-          let context = el.find("div.context").text();
-          if (context === "Skills Pass-Off") {
+          //different rules depending on assignment group
+          let assignmentGroupName = el.find("div.context").text();
+          if (assignmentGroupName === "Skills Pass-Off") {
             let rubricAssessment = {};
             let rubricAssessments = ENV.rubric_assessments;
             //rewrite this to do a filter on the array, sort so the most recent is first, then set that as the assessment
             for (let a in rubricAssessments) {
               let rub = rubricAssessments[a];
               if (rub.rubric_association.association_id == assignment.id) {
-                console.log(rub);
+                if (perc < 1) {
+                  console.log(rub);
+                }
                 rubricAssessment = rub;
               }
             }
