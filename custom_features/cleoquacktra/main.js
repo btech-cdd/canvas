9a9e42079085cda22bc3cd98211d417fd4cd90ed
Joshua Hveem <jhveem@btech.edu>
2023-02-15 08:27:15 -0700

    cleoquacktra image

--- custom_features/cleoquacktra/main.js
+++ custom_features/cleoquacktra/main.js
@@ -173,7 +173,7 @@
             <div
               class="msg-img"
               :style="{
-                'background-image': 'url(\"https://bridgetools.dev/canvas/media/cleoquacktra.gif\")' 
+                'background-image': 'url(\'https://bridgetools.dev/canvas/media/cleoquacktra.gif\')' 
               }"
             ></div>
 
