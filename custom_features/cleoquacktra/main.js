f5a7c8b0845a5a1b04ac22976eb08867cf39ed9f
Joshua Hveem <JoshuaH@btech.edu>
2023-02-14 20:35:49 -0700

    cleoquacktra

--- custom_features/cleoquacktra/main.js
+++ custom_features/cleoquacktra/main.js
@@ -70,9 +70,9 @@
 
                 <main class="msger-chat">
                 </main>
-                <form class="msger-inputarea">
+                <form @submit.prevent="submitRequest" class="msger-inputarea">
                   <input type="text" class="msger-input" placeholder="Enter your message...">
-                  <button @click="submitRequest" type="submit" class="msger-send-btn">Ask</button>
+                  <button type="submit" class="msger-send-btn">Ask</button>
                 </form>
             </div>
           </div>
