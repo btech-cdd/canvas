f14c682ae2649323de0b85db8cf592e7e5a7498b
Joshua Hveem <JoshuaH@btech.edu>
2023-02-14 20:18:59 -0700

    cleoquacktra

--- custom_features/cleoquacktra/main.js
+++ custom_features/cleoquacktra/main.js
@@ -1,20 +1,142 @@
-$("body").append(`
-  <div id="vue-cleoquacktra">
-    <p>This is a test</p>
-  </div>
-`);
-new Vue({
-  el: '#vue-cleoquacktra',
-  mounted: async function () {
-  },
-
-  data: function () {
-    return {
-      history: []
-    }
-  },
-  computed: {
-  },
-  methods: {
+(function () {
+  if ($("#cleoquacktra").length === 0) {
+      let vueString = `
+        <div id="cleoquacktra">
+          <div 
+            @mouseover="buttonX = 100;"
+            @mouseleave="buttonX = 10;"
+            @click="showHelp = !showHelp; console.log(showHelp);"
+            style='
+              width: 110px; 
+              margin-right: -140px; 
+              position:fixed; 
+              bottom: 20px; 
+              z-index:1000; 
+              transition: 0.5s; 
+              background-color: #A00012;
+              border: 2px solid #D61310;
+              padding: 10px 20px;
+              color: #FFF;
+              border-radius: 5px; 
+              cursor: pointer;
+              user-select: none;
+            ' 
+            :style="{'right': buttonX + 'px'}"
+          >
+          Need Help?
+          </div>
+          <div v-if="showHelp">
+            <div
+              @click.self="showHelp = false;"
+              style='
+                position: fixed;
+                z-index: 100000;
+                padding-top: 10px;
+                left: 0;
+                top: 0;
+                width: 100%;
+                height: 100%;
+                overflow: auto;
+                background-color: #000;
+                background-color: rgb(0, 0, 0, 0.4);
+              ' 
+            >
+            <div
+              id="btech-help-modal"
+              style='
+                position: absolute;
+                background-color: rgb(255,255,255);
+                color: #000;
+                width: 80%;
+                height: 80%;
+                top: 10%;
+                left: 10%;
+                z-index: 100001;
+                border-radius: 5px;
+                box-shadow: 0px 0px 8px 1px #333;
+                padding: 10px;
+                overflow-y: scroll;
+              '
+            >
+              <div v-for="(topic, key) in topics">
+                <help-topic :topic='topic' :name='key'>
+                </help-topic>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      `;
+      $('body').append(vueString);
+      class Resource {
+        constructor(name, target, topic) {
+          this.name = name;
+          this.target = target;
+          this.topic = topic;
+          this.url = '';
+          this.kalturaId = '';
+          this.questions = [];
+        }
+        addQuestion(question) {
+          this.questions.push(question);
+        }
+      }
+      //IMPORTED_FEATURE._init();
+      new Vue({
+        el: "#cleoquacktra",
+        mounted: async function() {
+          this.resources = [];
+        },
+        computed: {
+        },
+        data: function() {
+          return {
+            buttonX: 10,
+            showHelp: false,
+            topics: {},
+          }
+        }
+      });
+      Vue.component('help-topic', {
+        template: `
+          <div>
+            <h2 @click="show = !show" style="
+              background-color: #A00012;
+              border-radius: 4px;
+              color: #FFF;
+              cursor: pointer;
+              user-select: none;
+            ">
+              <i v-if="show" class='icon-mini-arrow-down'></i>
+              <i v-else class='icon-mini-arrow-right'></i>
+              {{name}}
+            </h2>
+            <div v-show="show" style="padding: 10px;">
+              <div v-for="(resource, index) in topic" style="border-bottom: 2px dotted #000">
+                <p>
+                  <div v-if="resource.url !== ''">
+                    <a target="_blank" :href="resource.url">{{resource.name}}</a>
+                  </div>
+                  <div v-else>
+                    {{resource.name}}
+                  </div>
+                </p>
+                <div style="text-align: center;">
+                  <iframe v-if="resource.kalturaId !== ''" :src="'https://cdnapisec.kaltura.com/p/1699651/sp/169965100/embedIframeJs/uiconf_id/22825111/partner_id/1699651?iframeembed=true&entry_id='+resource.kalturaId" width="400" height="333" allowfullscreen webkitallowfullscreen mozAllowFullScreen allow="autoplay *; fullscreen *; encrypted-media *" frameborder="0"></iframe>
+                </div>
+              </div>
+            </div>
+          </div>
+        `,
+        data: function() {
+          return {
+            show: false
+          }
+        },
+        props: [
+          'topic',
+          'name'
+        ]
+      });
   }
-});
+})();
