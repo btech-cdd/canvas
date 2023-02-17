1e5ed1a8d961459a8a082002b15fb547f75afdc6
Joshua Hveem <jhveem@btech.edu>
2023-02-17 15:01:40 -0700

    fixing not showing

--- custom_features/cleoducktra/quiz-questions.js
+++ custom_features/cleoducktra/quiz-questions.js
@@ -103,7 +103,7 @@
             "max_tokens": 2000,
             "top_p": 1,
             "frequency_penalty": 0,
-            "presence_penalty": 0.6,
+            "presence_penalty": 0,
             "stop": [" Human:", " AI:"]
           }`;
           let response = "";
