3603097d0346f4f00cd5b023cb2538a30938bd57
Joshua Hveem <jhveem@btech.edu>
2023-02-17 16:31:24 -0700

    create questions

--- custom_features/cleoducktra/quiz-questions.js
+++ custom_features/cleoducktra/quiz-questions.js
@@ -24,13 +24,12 @@
             <main class="msger-chat">
               <div>Create a quiz question about...</div>
             </main>
-            <form 
+            <div
               style="margin: 0;"
-              @submit.prevent="submitRequest" 
               class="msger-inputarea">
               <input v-model="input" type="text" class="msger-input" placeholder="Enter your message...">
-              <button type="submit" class="msger-send-btn">Ask</button>
-            </form>
+              <button @click="submitRequest();" class="msger-send-btn">Ask</button>
+            </div>
           </div>
           <div
             class="msger btech-modal-content"
@@ -46,13 +45,13 @@
                 <p>Correct answer: {{question.correct + 1}}</p>
               </div>
             </main>
-            <form 
+            <div
               style="margin: 0;"
               class="msger-inputarea">
               <button @click="createQuestion(); submitRequest();" class="msger-send-btn">Create</button>
               <button @click="submitRequest();" class="msger-send-btn blue">Next</button>
               <button @click="state = 'prompt'; input='';" class="msger-send-btn red">Restart</button>
-            </form>
+            </div>
           </div>
         </div>
       </div>
@@ -120,7 +119,6 @@
           }); 
         },
         submitRequest: async function() {
-          console.log("SUBMIT!")
           let input = this.input;
           $.ajaxSetup({
             headers:{
