c800dd0f94bef77e3ef078f075f87b55bc02ca34
Joshua Hveem <jhveem@btech.edu>
2023-02-17 14:10:31 -0700

    building quiz question gen

--- custom_features/cleoducktra/quiz-questions.js
+++ custom_features/cleoducktra/quiz-questions.js
@@ -19,7 +19,7 @@
               style="margin: 0;"
               @submit.prevent="submitRequest" 
               class="msger-inputarea">
-              <div>Create a question about...</div>
+              <div>Create a question about...</div><br>
               <input v-model="input" type="text" class="msger-input" placeholder="Enter your message...">
               <button :disabled="awaitingResponse" type="submit" class="msger-send-btn">Ask</button>
             </form>
