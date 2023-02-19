dcb0290f74b2c586457696fbf74f4e0efd62f06d
Joshua Hveem <JoshuaH@btech.edu>
2023-02-18 19:29:08 -0700

    submit on enter

--- custom_features/cleoducktra/main.vue
+++ custom_features/cleoducktra/main.vue
@@ -20,7 +20,7 @@
               'selected': menuCurrent == menu
             }"
           >{{menu}}</div>
-          <span style="float: right; color: #FFF; cursor: pointer;" @click="show = false;">X</span>
+          <span style="padding: 0.5rem; float: right; color: #FFF; cursor: pointer;" @click="show = false;">X</span>
         </div>
         <!--Ask Cleo-->
         <div
