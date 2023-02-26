8b967b273510665571dd5cdf8b8bbb687488ce77
Joshua Hveem <JoshuaH@btech.edu>
2023-02-26 08:56:24 -0700

    include new lines in git

--- custom_features/editor_toolbar/main.js
+++ custom_features/editor_toolbar/main.js
@@ -16,6 +16,7 @@
       try {
         let resp = await $.get(`/api/v1/users/self/custom_data/openaiapikey?ns=dev.bridgetools.rce`);
         let key = resp.data;
+        this.aiRegisterConfig.initialData.key = key;
         RCE.openAIKey = key;
       } catch (err) {
         RCE.openAIKey = "";
