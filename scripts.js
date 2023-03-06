4819557af844c34bfb81206d7a60ddebb11df94c
Joshua Hveem <jhveem@btech.edu>
2023-03-06 09:38:27 -0700

    use awaits instead of .done

--- scripts.js
+++ scripts.js
@@ -1,8 +1,3 @@
-var SOURCE_URL = 'https://bridgetools.dev/canvas'
-if (BETA) {
-  SOURCE_URL = 'https://bridgetools.dev/canvas-beta'
-}
-
 async function delay(ms) {
   // return await for better async stack trace support in case of errors.
   return await new Promise(resolve => setTimeout(resolve, ms));
