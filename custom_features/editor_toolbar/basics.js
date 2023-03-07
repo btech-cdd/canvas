c71381d5e6e790fccde68fb119173471cf7f4056
Joshua Hveem <jhveem@btech.edu>
2023-03-07 11:12:15 -0700

    turn toolbar back on for a sec

--- custom_features/editor_toolbar/basics.js
+++ custom_features/editor_toolbar/basics.js
@@ -241,8 +241,10 @@
     });
   }
 
-  await TOOLBAR.checkReady();
+  console.log(TOOLBAR.initted);
+  await TOOLBAR.checkReady(0);
   console.log(TOOLBAR.toolbar);
+  console.log(TOOLBAR.initted);
 
   //Add in option to change color of exampleBox. IE, you click in it, it figures out he color selected, if you change the color, it changes the box
   TOOLBAR.toolbar.prepend(`<input type="color" id="btech-custom-editor-buttons-color" value="#d22232" style="width: 48px; padding: 4px; padding-right: 0px;" list="default-colors"/>
