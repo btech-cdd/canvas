122203ad837ba3a7992e78915fa1bc6bd5cd9928
Joshua Hveem <jhveem@btech.edu>
2023-03-27 16:59:02 -0600

    use textarea

--- custom_features/cleoducktra/main.js
+++ custom_features/cleoducktra/main.js
@@ -214,8 +214,8 @@
           });
         },
         autoResizeInput(e) {
+          console.log(e);
           const textarea = $(e.target);
-          console.log(this.$refs);
           textarea.style.height = "auto";
           const maxHeight = 200; // set the maximum height here
           textarea.style.height = (textarea.scrollHeight + 2) + "px";
