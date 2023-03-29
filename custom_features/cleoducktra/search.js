8ffdf7768a4e242eeb5451eb50bf0db830ac00e3
Joshua Hveem <jhveem@btech.edu>
2023-03-29 15:54:35 -0600

    adding search functionality

--- custom_features/cleoducktra/search.js
+++ custom_features/cleoducktra/search.js
@@ -69,7 +69,7 @@
             let resp = await CLEODUCKTRA.get(`
               Does the following content relate to my query: ${input}?
               If no, respond with just one word, 'No'. 
-              If yes, provide useful information based on the content and provide a quote from the content to support your answer.
+              If yes, provide useful information based on the content and provide a quote from the content to support your answer. Don't actually say Yes though.
               CONTENT: ${page}
             `)
             console.log(resp);
