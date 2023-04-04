b7cdf11bb50789d613a1c911da794dfb61478ae0
Joshua Hveem <jhveem@btech.edu>
2023-04-04 12:13:18 -0600

    brick wall

--- custom_features/quizzes/upload_questions.js
+++ custom_features/quizzes/upload_questions.js
@@ -23,7 +23,7 @@ $("body").append(`
         class='btech-modal-content-inner'
       >
         <div v-for="file in files">
-          <span>{{file.name}}</span><span>{{uploadProgress[file.name]}}%</span>
+          <span>{{file.name}}</span><span>{{Math.round(uploadProgress[file.name] * 100)}}%</span>
         </div>
       </div>
     </div>
