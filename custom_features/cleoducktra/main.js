f12111ab202de48d411291b75b8a3e798f071234
Joshua Hveem <jhveem@btech.edu>
2023-05-02 11:43:46 -0600

    update

--- custom_features/cleoducktra/main.js
+++ custom_features/cleoducktra/main.js
@@ -81,7 +81,7 @@
           }
         ],
         "temperature": 0.9,
-        "max_tokens": 2000,
+        "max_tokens": 8000,
         "top_p": 1,
         "frequency_penalty": 0,
         "presence_penalty": 0.6,
