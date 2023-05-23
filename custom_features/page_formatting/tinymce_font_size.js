ebca6dddc505380bc6d7a80c37f6efbc4939312e
Joshua Hveem <jhveem@btech.edu>
2023-05-23 11:39:34 -0600

    attempting to fix css for editor

--- custom_features/page_formatting/tinymce_font_size.js
+++ custom_features/page_formatting/tinymce_font_size.js
@@ -1,9 +1,13 @@
 TOOLBAR_STYLES = {
   init: async function () {
-    //save current settings so you don't lose anything Canvas has set up
+    let css = $.get("https://bridgetools.dev/canvas/style/rce.css");
+    console.log(css);
+    return
+    //THIS DOESN'T WORK
     let savedSettings = tinymce.activeEditor.settings;
     //save the setup function
     //create a new setup function that first calls the old one, then adds whatever button you want (or other settings)
+    //save current settings so you don't lose anything Canvas has set up
     savedSettings.content_css.push("https://bridgetools.dev/canvas/style/rce.css");
     //get rid of the current editor
     tinymce.activeEditor.destroy();
