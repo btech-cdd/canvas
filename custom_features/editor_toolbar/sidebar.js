e47999b6492484e05e63bfcd7fd78e4349219a81
Joshua Hveem <jhveem@btech.edu>
2023-06-09 15:56:58 -0600

    copy to next year

--- custom_features/editor_toolbar/sidebar.js
+++ custom_features/editor_toolbar/sidebar.js
@@ -3,16 +3,22 @@
   $("#wrapper").css("margin-right", `${WIDTH}px`);
   $("body").append(`
   <div 
-  id="btech-editor-vue"
-  style="position: fixed; top: 0; right: 0; width: ${WIDTH}px; height: 100%; background-color: #f1f1f1;">
-    <div style="text-align: center; background-color: #d22232; color: white;">
-      BTECH Editor
-      <span 
-        @click="minimize"
-        style="float: right; padding-right: 1rem; cursor: pointer; user-select: none;"
-      >
-        <b>&#8250;</b></span>
+    id="btech-editor-vue"
+    style="position: fixed; top: 0; right: 0; width: ${WIDTH}px; height: 100%; background-color: #f1f1f1;"
+  >
+    <div>
+      <div style="text-align: center; background-color: #d22232; color: white;">
+        BTECH Editor
+        <span 
+          @click="minimize"
+          style="float: right; padding: 0 1rem; cursor: pointer; user-select: none;"
+        >
+          <b>&#8250;</b>
+        </span>
       </div>
+    </div>
+    <div>
+    </div>
   </div>
   `);
   new Vue({
@@ -21,6 +27,7 @@
     },
     data: function () {
       return {
+        minimized: false;
       }
     },
     methods: {
