a93f98b92231cd22c440d14dcffc2a0d8d3eaf79
Joshua Hveem <jhveem@btech.edu>
2023-06-09 15:53:46 -0600

    copy to next year

--- custom_features/editor_toolbar/sidebar.js
+++ custom_features/editor_toolbar/sidebar.js
@@ -5,7 +5,7 @@
   <div 
   id="btech-editor-vue"
   style="position: fixed; top: 0; right: 0; width: ${WIDTH}px; height: 100%; background-color: #f1f1f1;">
-    <div style="text-align: center; background-color: #d22232; color: white;">BTECH Editor<span style="float: right;">&#8250;</span></div>
+    <div style="text-align: center; background-color: #d22232; color: white;">BTECH Editor<span style="float: right;"><b>&#8250;</b></span></div>
   </div>
   `);
   new Vue({
