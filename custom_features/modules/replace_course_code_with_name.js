00e8250f50b70d91e3670eae84f0301dd9e147dd
Joshua Hveem <jhveem@btech.edu>
2023-06-22 10:58:26 -0600

    replace course code with name

--- custom_features/modules/replace_course_code_with_name.js
+++ custom_features/modules/replace_course_code_with_name.js
@@ -1,2 +1,4 @@
-let courseData = await $.get("/api/v1/courses/" + ENV.COURSE_ID);
-$(`a[href$="/courses/${ENV.COURSE_ID}"] span`).html(courseData.name);
+(async function() {
+  let courseData = await $.get("/api/v1/courses/" + ENV.COURSE_ID);
+  $(`a[href$="/courses/${ENV.COURSE_ID}"] span`).html(courseData.name);
+})();
