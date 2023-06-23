630eba3e6f9a0e3187947230dc84d8071a1b676e
Joshua Hveem <jhveem@btech.edu>
2023-06-23 11:33:21 -0600

    add data

--- custom_features/modules/enrollment_dates_student.js
+++ custom_features/modules/enrollment_dates_student.js
@@ -19,6 +19,7 @@ var Countdown = {
     if (this.enrollment.end_at == undefined) {
       let sectionURL = `/api/v1/courses/${ENV.COURSE_ID}/sections/${this.enrollment.course_section_id}`;
       let section = (await $.get(sectionURL))
+      console.log(section);
       this.enrollment.end_at = section.end_at;
     }
     let checkDepartment = this.enabledDepartments.includes(CURRENT_DEPARTMENT_ID);
