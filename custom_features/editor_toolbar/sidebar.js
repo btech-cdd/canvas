189734d3d0f7976d7fcf8b499800903bc012c7bc
Joshua Hveem <JoshuaH@btech.edu>
2023-07-09 17:43:37 -0600

    right image modal

--- custom_features/editor_toolbar/sidebar.js
+++ custom_features/editor_toolbar/sidebar.js
@@ -54,6 +54,14 @@
           class="icon-text"
         ></i>
         <i
+          @click="addHeaderBanner"
+          class="icon-text"
+        ></i>
+        <i
+          @click="addHeaderHex"
+          class="icon-text"
+        ></i>
+        <i
           @click="addImageRightModal"
           class="icon-image"
         ></i>
@@ -85,10 +93,24 @@
       return {
         minimized: false,
         width: 250,
-        defaultImg: 'https://bridgetools.dev/canvas/media/image-placeholder.png'
+        defaultImg: 'https://bridgetools.dev/canvas/media/image-placeholder.png',
+        colors: {
+          primary: "#D22232",
+          secondary: "#B11121",
+          callout: "#F1F1F1",
+          font: "#FFFFFF",
+          bodyfont: "#000000",
+          bg: "#FFFFFF"
+        } 
       }
     },
     methods: {
+      setCSSVariables: function () {
+        document.querySelector(':root').style.setProperty('--font', 'Roboto');
+        for (let color in this.colors) {
+          document.querySelector(':root').style.setProperty('--color-' + color, this.colors[color]);
+        }
+      },
       maximize: function () {
         $('#wrapper').css('margin-right', this.width + 'px');
         this.minimized = false;
@@ -194,6 +216,61 @@
           </div>
         `)
         console.log(container);
+      },
+
+      addHeaderBannerModal: function () {
+
+      },
+
+      addHeaderHexModal: function () {
+        let editor = tinymce.activeEditor;
+        let body = editor.getBody();
+        this.initFormattedContent();
+        let wrapper = $($(body).find('.btech-formatted-content-wrapper')[0]);
+        wrapper.prepend(`
+          <div
+            class="
+              btech-formatted-content-modal
+              btech-formatted-content-banner-wrapper
+            "
+            style="
+              width: 100%;
+            " 
+          >
+            <div
+              style="
+                width: 100%;
+                height: 10rem;
+                overflow: hidden;
+                position: relative;
+                z-index: 1;
+              " 
+            >
+              <img 
+                style="
+                  width:100%;
+                "
+                src="${this.defaultImg}"
+              >
+            </div>
+
+            <h2
+              style="
+                margin-top: -2rem;
+                background-color: var(--colors-primary);
+                color: var(--colors-font);
+                position: relative;
+                z-index: 2;
+                font-size: 2rem;
+                display: inline-block;
+                margin-left: -2rem;
+                padding-left: 3rem;
+                width: 90%;
+                border: 0.25rem solid #FFFFFF;
+              " 
+            ><strong>HEADER</strong></h2>
+          </div>
+        `);
       },
 
       addBannerModal: function () {
