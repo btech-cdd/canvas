31810a7e870ea8c3b1de7e22933a2153c5d4d11a
Joshua Hveem <jhveem@btech.edu>
2023-07-20 15:44:57 -0600

    cours evaluation

--- custom_features/course_evaluation.js
+++ custom_features/course_evaluation.js
@@ -151,10 +151,10 @@
       // may be more accurate to pull based on sis course id 
       let sisCourseId = courseData.sis_course_id;
       if (sisCourseId == undefined) return; //don't do anything, no need to rate?
-      const yearPattern = /(\d{4})[A-Z]{2}/;
+      const yearPattern = /(\d{4})[A-Z]{2}$/;
       const courseCodePattern = /[A-Z]{4} \d{4}/;
 
-      const year = sisCourseId.match(yearPattern);
+      const year = sisCourseId.match(yearPattern)[1];
       console.log(year);
       const courseCode = sisCourseId.match(courseCodePattern)[0];
       console.log(courseCode);
