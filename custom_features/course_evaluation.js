4869711f7a5794a700912381ad0ee72145f11aac
Joshua Hveem <jhveem@btech.edu>
2023-08-03 14:05:53 -0600

    pin url

--- custom_features/course_evaluation.js
+++ custom_features/course_evaluation.js
@@ -160,7 +160,7 @@
               ><b>{{i}}</b></span>
             </div>
             <div>
-                <textarea style="margin-top: 0.25rem; height: 1rem; box-sizing: border-box; resize: none; width: 100%;"></textarea>
+                <textarea style="margin-top: 0.5rem; height: 2rem; box-sizing: border-box; resize: none; width: 100%;"></textarea>
             </div>
           </div>
         </div>
