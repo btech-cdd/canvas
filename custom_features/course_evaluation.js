e1173e94f750d69ce552853098f3270d9e610806
Joshua Hveem <jhveem@btech.edu>
2023-08-04 14:48:42 -0600

    pin url

--- custom_features/course_evaluation.js
+++ custom_features/course_evaluation.js
@@ -372,15 +372,11 @@
       },
       setComment: async function (scoreId, comment) {
         this.updating = true;
-        this.$nextTick(async () => {
-          console.log("COMMENT")
-          console.log(comment);
-          let score = await bridgetoolsReq(`https://reports.bridgetools.dev/api/reviews/scores/${scoreId}`, {
-            comment: comment 
-          }, "PUT");
-          console.log(score);
-          this.updating = false;
-        });
+        let score = await bridgetoolsReq(`https://reports.bridgetools.dev/api/reviews/scores/${scoreId}`, {
+          comment: comment 
+        }, "PUT");
+        console.log(score);
+        this.updating = false;
       },
       pinURL: async function (scoreId, currentURL) {
         let url = window.location.origin + window.location.pathname;
