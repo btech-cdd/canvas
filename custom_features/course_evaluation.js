cf45bf8e8ed2e195336ecc88e82716d2211296a1
Joshua Hveem <jhveem@btech.edu>
2023-09-01 09:35:12 -0600

    past review

--- custom_features/course_evaluation.js
+++ custom_features/course_evaluation.js
@@ -212,7 +212,7 @@
             "
           >
             <div>
-              <span style="width: 6rem;">{{bridgetools.dateToString(review.date)}}</span>
+              <span style="display: inline-block; width: 6rem;">{{bridgetools.dateToString(review.date)}}</span>
               <span style="font-size: 0.75;"><i>{{raterNames?.[review.rater_id] ?? ""}}</i></span>
               <span
                 style="
