e55714623fb92aade2babb860227a0b2c27a0ad8
Joshua Hveem <jhveem@btech.edu>
2023-09-06 08:28:26 -0600

    quiz to page

--- custom_features/modules/convert_to_page.js
+++ custom_features/modules/convert_to_page.js
@@ -1,36 +1,59 @@
-(function () {
-  function convertAssignmentToPage(courseId, moduleId, item) {
-    event.preventDefault();
+(async function () {
+  async function convertAssignmentToPage(courseId, moduleId, item) {
+    // event.preventDefault();
     let itemId = item.find("div.ig-admin span").attr("data-content-id");
     let moduleItemId = item.find("div.ig-admin span").attr("data-module-item-id");
-    $.get("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items/" + moduleItemId, function (data) {
-      let oldModuleItemData = data;
-      $.get("/api/v1/courses/" + courseId + "/assignments/" + itemId, function (data) {
-        $.post("/api/v1/courses/" + courseId + "/pages", {
-          wiki_page: {
-            title: data.name,
-            body: data.description,
-            published: oldModuleItemData.published
-          }
-        }, function (data) {
-          $.post("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items", {
-            module_item: {
-              title: data.title,
-              type: 'Page',
-              position: oldModuleItemData.position,
-              indent: oldModuleItemData.indent,
-              page_url: data.url
-            }
-          }, function (data) {
-            $.delete("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items/" + moduleItemId).done(function () {
-              $.delete("/api/v1/courses/" + courseId + "/assignments/" + oldModuleItemData.content_id).done(function () {
-                location.reload(true);
-              });
-            });
-          });
-        });
-      });
+    let oldModuleItemData = await $.get("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items/" + moduleItemId);
+    let assignment = await $.get("/api/v1/courses/" + courseId + "/assignments/" + itemId);
+    let page = await $.post("/api/v1/courses/" + courseId + "/pages", {
+      wiki_page: {
+        title: assignment.name,
+        body: assignment.description,
+        published: oldModuleItemData.published
+      }
     });
+    await $.post("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items", {
+      module_item: {
+        title: page.title,
+        type: 'Page',
+        position: oldModuleItemData.position,
+        indent: oldModuleItemData.indent,
+        page_url: page.url
+      }
+    });
+    await $.delete("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items/" + moduleItemId);
+    await $.delete("/api/v1/courses/" + courseId + "/assignments/" + oldModuleItemData.content_id);
+    location.reload(true);
+  }
+  async function convertQuizToPage(courseId, moduleId, item) {
+    // event.preventDefault();
+    let itemId = item.find("div.ig-admin span").attr("data-content-id");
+    let moduleItemId = item.find("div.ig-admin span").attr("data-module-item-id");
+    let oldModuleItemData = await $.get("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items/" + moduleItemId);
+    console.log(oldModuleItemData);
+    return
+    let quiz = await $.get("/api/v1/courses/" + courseId + "/quizzes/" + itemId);
+    console.log(quiz);
+    let page = await $.post("/api/v1/courses/" + courseId + "/pages", {
+      wiki_page: {
+        title: assignment.name,
+        body: assignment.description,
+        published: oldModuleItemData.published
+      }
+    });
+    await $.post("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items", {
+      module_item: {
+        title: page.title,
+        type: 'Page',
+        position: oldModuleItemData.position,
+        indent: oldModuleItemData.indent,
+        page_url: page.url
+      }
+    });
+    await $.delete("/api/v1/courses/" + courseId + "/modules/" + moduleId + "/items/" + moduleItemId);
+    await $.delete("/api/v1/courses/" + courseId + "/assignments/" + oldModuleItemData.content_id);
+    location.reload(true);
   }
   addToModuleItemMenu("Convert To Page", "Remove this item from the module", convertAssignmentToPage, "Assignment");
+  addToModuleItemMenu("Convert To Page", "Remove this item from the module", convertAssignmentToPage, "Quiz");
 })();
