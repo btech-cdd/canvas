01098ae01fc168255a65ff26f11fc7dd889e6333
Joshua Hveem <jhveem@btech.edu>
2023-09-26 15:17:31 -0600

    course pacing

--- custom_features/people_page/sync_start_dates_with_section.js
+++ custom_features/people_page/sync_start_dates_with_section.js
@@ -24,7 +24,7 @@
       }
     }
   })()
-  let picker = $("body").find('span[dir="ltr"]')[1];
+  let picker = $("body");
   observeDOM( picker, function(m){ 
     console.log("CHANGE")
       $($("div[data-testid='coursepace-end-date']")[0]).parent().after(`<span><b>Date Override</b><br><input id="date-override" type="date" value=""></span>`);
