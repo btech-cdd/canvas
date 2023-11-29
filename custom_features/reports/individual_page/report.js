c02813c9d1f7cb4b76e764cc81c29d6a3cf9b51c
Joshua Hveem <jhveem@btech.edu>
2023-11-29 09:50:58 -0700

    fixing...

--- custom_features/reports/individual_page/report.js
+++ custom_features/reports/individual_page/report.js
@@ -25,7 +25,7 @@
                   <h4 class="ic-notification__title">
                     High School Students!
                   </h4>
-                  <span class="notification_message">The grade here may <strong>NOT</strong> be your final grade for the term. Your final grade will be based on only the assignments submitted during the term and may take into account other factors such as the ammount of work you completed and/or work completed in other courses. Contact your instructor if you have questions about how your grade will be calculated.</span></div>
+                  <p class="notification_message">The grade here is your course grade, but may <strong>NOT</strong> be your final grade submitted to your high school at the end of the term. Your term grade will be based on only the assignments submitted during the term and may be based on the ammount of work you completed across multiple courses.</p><p>Contact your instructor if you have questions about how your grade will be calculated.</p>
               </div>
             </div>
           `);
