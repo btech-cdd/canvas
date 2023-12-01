10697309b613eb9c85b9d42e4d5b8d56ac420034
Joshua Hveem <jhveem@btech.edu>
2023-12-01 12:47:23 -0700

    fix text entry for assignments

--- custom_features/reports/accreditation.js
+++ custom_features/reports/accreditation.js
@@ -429,8 +429,8 @@
             let title = data.assignment.name + "-" + data.submission.user.name + " submission rubric"
             let commentEl = app.getComments(data.submission);
             content.find("#rubric_holder").show();
-            console.log(content.submission);
-            content.find('#rubric_holder').prepend(`<div>${content.submission.body}</div>`);
+            console.log(data.submission);
+            content.find('#rubric_holder').prepend(`<div>${data.submission.body}</div>`);
             content.find("#rubric_holder").prepend("<div>Submitted:" + data.submission.submitted_at + "</div>");
             content.find("#rubric_holder").prepend("<div>Student:" + data.submission.user.name + "</div>");
             content.find("#rubric_holder").prepend("<div>Title:" + data.assignment.name + "</div>");
