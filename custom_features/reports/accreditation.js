5e2d09b6ced29b45d3601a0657b6169d4f0a722d
Joshua Hveem <jhveem@btech.edu>
2023-12-01 13:06:40 -0700

    fix text entry for assignments

--- custom_features/reports/accreditation.js
+++ custom_features/reports/accreditation.js
@@ -510,7 +510,7 @@
           async createIframe(url, func = null, data = {}) {
             let id = genId();
             let elId = 'btech-content-' + id
-            let iframe = $('<iframe id="' + elId + '" style="display: none; width: 800px;" src="' + url + '"></iframe>');
+            let iframe = $('<iframe id="' + elId + '" style="display: none; width: 1200px;" src="' + url + '"></iframe>');
 
             $("#content").append(iframe);
             //This is unused. was for trying to convert an html element to a canvas then to a data url then to image then to pdf, but ran into cors issues.
