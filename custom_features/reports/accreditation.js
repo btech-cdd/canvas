e6aacfeaa43c7a5bb96d82784c78351afe293746
Joshua Hveem <jhveem@btech.edu>
2024-02-01 15:18:53 -0700

    fixing iframe issue

--- custom_features/reports/accreditation.js
+++ custom_features/reports/accreditation.js
@@ -477,7 +477,7 @@
             window.onafterprint = (event) => {
               $('title').text(ogTitle);
               app.preparingDocument = false;
-              // iframe.remove();
+              iframe.remove();
             }
             window.focus();
             // setTimeout(function() { window.print(); }, 5000);
