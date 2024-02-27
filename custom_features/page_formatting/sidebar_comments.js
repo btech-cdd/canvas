b3a6890ab0c70955d820be3b499e0753cd26c8c8
Joshua Hveem <jhveem@btech.edu>
2024-02-27 09:45:28 -0700

    unneeded media

---
+++ custom_features/page_formatting/sidebar_comments.js
@@ -0,0 +1,61 @@
+/*
+  IF THERE ARE ANY SIDEBAR COMMENTS
+    REFORMAT THE PAGE SO THERE'S A WIDER PADDING
+    MOVE COMMENTS TO THE SIDE
+    ADD A ON RESIZE FUNCTION TO ADJUST THE TOP OF THE COMMENT TO THE CONTENT'S HEIGHT
+
+  TO DO
+    DOESN'T HANDLE MULTIPLE COMMENTS
+*/
+(async function() {
+
+  // Function to align the callout box with the paragraph
+  function alignCallout() {
+      var contents = $('.btech-sidebar-content');
+      if (contents.length == 0) return;
+
+      // if we actually have something here, then reformat the page
+      var container = $('.show-content'); // Get the container
+
+      contents.each(function () {
+          let content = $(this);
+          var classes = content.attr('class').split(/\s+/); // Split classes into an array
+          var contentId;
+
+          // Find the class that matches the pattern
+          $.each(classes, function(index, item) {
+              if (item.startsWith('btech-sidebar-content-')) {
+                  contentId = item.replace('btech-sidebar-content-', ''); // Extract the <id>
+                  return false; // Break the loop
+              }
+          });
+
+          if (contentId) {
+              // Move the corresponding comment
+              var commentSelector = '.btech-sidebar-comment-' + contentId;
+              var comment = $(commentSelector);
+
+              // Calculate the top position relative to the container
+              let contentTop = content.offset().top - container.offset().top;
+              comment.css('position', 'absolute'); // Ensure the comment is positioned absolutely within the container
+              comment.css('top', contentTop + 'px');
+              comment.css('left', '0px');
+              comment.css('width', '150px');
+          }
+      });
+  }
+
+  //make sure there's actualy a sidebar comment to even work with before adding crap in
+  var contents = $('.btech-sidebar-content');
+  if (contents.length > 0) {
+    // if we actually have something here, then reformat the content box to have padding on the left 
+    var container = $('.show-content'); // Get the container
+    container.css('position', 'relative');
+    container.css('padding-left', '150px');
+    // Align the callout on initial load
+    alignCallout();
+
+    // Re-align the callout box on window resize
+    $(window).on('resize', alignCallout);
+  }
+})();
