284d2fc6f77d540711bbc7cd3dceb13944531591
Joshua Hveem <jhveem@btech.edu>
2024-02-27 09:55:33 -0700

    unneeded media

--- custom_features/page_formatting/sidebar_comments.js
+++ custom_features/page_formatting/sidebar_comments.js
@@ -46,7 +46,7 @@
                 , 'border': '1px solid red'
                 , 'border-radius': '5px'
                 , 'padding': '5px'
-                , 'background-color': 'light-red'
+                , 'background-color': '#FEE' 
               });
           }
       });
