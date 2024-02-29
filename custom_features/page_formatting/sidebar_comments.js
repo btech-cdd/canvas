d4df7c01c4db99f9902840d21bfe6dd827502e2a
Joshua Hveem <jhveem@btech.edu>
2024-02-29 08:10:56 -0700

    unneeded media

--- custom_features/page_formatting/sidebar_comments.js
+++ custom_features/page_formatting/sidebar_comments.js
@@ -45,7 +45,7 @@
               let highlightColor = "#F8F8F8";
               if (match) {
                 [ , r, g, b]= match.map(Number);
-                highlightColor = `rgba(${r}, ${g}, ${b}, 0.5)`
+                highlightColor = `rgba(${r}, ${g}, ${b}, 0.1)`
               }
 
               comment.css({
