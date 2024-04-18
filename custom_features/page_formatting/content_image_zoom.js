f39d4426a3e95491e8a13cdb4c6140621dcffe2d
Joshua Hveem <jhveem@btech.edu>
2024-04-18 08:23:50 -0600

    zoomed image click

--- custom_features/page_formatting/content_image_zoom.js
+++ custom_features/page_formatting/content_image_zoom.js
@@ -2,22 +2,25 @@
   console.log("image click")
   $("#content img").each(() => {
     let img = $(this);
-    img.click(() => {
-      console.log("CLICKY");
-      let modal = $(`
-        <div style="display: block;" class="btech-zoomed-image-modal">
-          <span class="btech-zoomed-image-modal-close">&times;</span>
-          <img class="btech-zoomed-image-modal-content" id="btech-zoomed-image-modal-img">
-        </div>
-      `);
-      $('#content').append(modal);
-      let src = img.attr("src");
-      $("#btech-zoomed-image-modal-img").attr("src", src);
+    if (!img.hasClass('btech-zoomed-image-modal-content')) {
+      img.click(() => {
+        if ($("#btech-zoomed-image-modal-img").length > 0) return;
+        console.log("CLICKY");
+        let modal = $(`
+          <div style="display: block;" class="btech-zoomed-image-modal">
+            <span class="btech-zoomed-image-modal-close">&times;</span>
+            <img class="btech-zoomed-image-modal-content" id="btech-zoomed-image-modal-img">
+          </div>
+        `);
+        $('#content').append(modal);
+        let src = img.attr("src");
+        $("#btech-zoomed-image-modal-img").attr("src", src);
 
-      $(".btech-zoomed-image-modal-close").click(function() {
-        console.log("close")
-        modal.remove();
+        $(".btech-zoomed-image-modal-close").click(function() {
+          console.log("close")
+          modal.remove();
+        });
       });
-    });
+    }
   });
 })();
