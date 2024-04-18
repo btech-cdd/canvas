96f622239042e04cd3864e9a2387d927268f676b
Joshua Hveem <jhveem@btech.edu>
2024-04-18 08:43:09 -0600

    zoomed image click

--- custom_features/page_formatting/content_image_zoom.js
+++ custom_features/page_formatting/content_image_zoom.js
@@ -1,13 +1,20 @@
 (function() {
   $(document).ready(function() {
     $("#content").on("click", "img:not(.btech-zoomed-image-modal-content)", function() {
+      let src = $(this).attr("src");
       let srcs = [];
+      let srcIndex = 0;
       $('#content img').each(function () {
-        console.log($(this).attr('src'));
-      })
+        let otherSource = $(this).attr('src');
+        if (src = otherSource) {
+          sourceIndex = srcs.length;
+        }
+        srcs.push(otherSource);
+      });
+      console.log(srcs);
+      console.log(srcIndex);
       if ($("#btech-zoomed-image-modal-img").length > 0) return;
       console.log("CLICKY");
-      let src = $(this).attr("src");
       let modal = $(`
         <div style="display: block;" class="btech-zoomed-image-modal">
           <span class="btech-zoomed-image-modal-close">&times;</span>
