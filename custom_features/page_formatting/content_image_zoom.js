32de15aa3609822634f4405a4443d8b21f310ae1
Joshua Hveem <jhveem@btech.edu>
2024-04-18 08:37:29 -0600

    zoomed image click

--- custom_features/page_formatting/content_image_zoom.js
+++ custom_features/page_formatting/content_image_zoom.js
@@ -5,10 +5,11 @@
       console.log("CLICKY");
       let src = $(this).attr("src");
       let modal = $(`
-        <div style="display: block;" class="btech-zoomed-image-modal">
-          <span class="btech-zoomed-image-modal-close">&times;</span>
-          <img class="btech-zoomed-image-modal-content" id="btech-zoomed-image-modal-img" src="${src}">
-        </div>
+        <div style="display: block; text-align: center; position: relative; height: 100vh;" class="btech-zoomed-image-modal">
+          <span class="btech-zoomed-image-modal-close" style="position: absolute; top: 10px; right: 10px; cursor: pointer;">×</span>
+          <img class="btech-zoomed-image-modal-content" id="btech-zoomed-image-modal-img" src="https://btech.instructure.com/courses/565875/files/108049322/preview" style="margin: auto; display: block;">
+      </div>
+
       `);
       $('#content').append(modal);
 
