b04aa5a437080276f8e488431212c68e4e0a6939
Joshua Hveem <jhveem@btech.edu>
2024-04-18 08:53:36 -0600

    fixing img zoomer

--- custom_features/page_formatting/content_image_zoom.js
+++ custom_features/page_formatting/content_image_zoom.js
@@ -14,40 +14,36 @@
       let modal = $(`
         <div style="display: block;" class="btech-zoomed-image-modal">
           <span class="btech-zoomed-image-modal-close">&times;</span>
-          <span class="btech-zoomed-image-modal-content-scroll-left" style="position: absolute; left: 10px; top: 50%; cursor: pointer; user-select: none;">&#9664;</span> <!-- Left arrow -->
+          <span class="btech-zoomed-image-modal-content-scroll-left" style="position: absolute; left: 10px; top: 50%; cursor: pointer; user-select: none;">&#9664;</span>
           <div style="text-align: center; position: relative;">
             <img class="btech-zoomed-image-modal-content" id="btech-zoomed-image-modal-img" src="${src}">
           </div>
-          <span class="btech-zoomed-image-modal-content-scroll-right" style="position: absolute; right: 10px; top: 50%; cursor: pointer; user-select: none;">&#9654;</span> <!-- Right arrow -->
+          <span class="btech-zoomed-image-modal-content-scroll-right" style="position: absolute; right: 10px; top: 50%; cursor: pointer; user-select: none;">&#9654;</span>
         </div>
       `);
       
       $('#content').append(modal);
 
       $('.btech-zoomed-image-modal-content-scroll-left').click(function(e) {
+        e.preventDefault();
         srcIndex -= 1;
         if (srcIndex < 0) srcIndex = srcs.length - 1;
-        src = srcs[srcIndex];
-        $('#btech-zoomed-image-modal-content-img').attr('src', src);
+        $('#btech-zoomed-image-modal-img').attr('src', srcs[srcIndex]);
         e.stopPropagation();
       });
 
-      $('.btech-zoomed-image-modal-scroll-right').click(function(e) {
+      $('.btech-zoomed-image-modal-content-scroll-right').click(function(e) {
+        e.preventDefault();
         srcIndex += 1;
         if (srcIndex >= srcs.length) srcIndex = 0;
-        console.log(srcIndex);
-        src = srcs[srcIndex];
-        console.log(src);
-        $('#btech-zoomed-image-modal-img').attr('src', src);
+        $('#btech-zoomed-image-modal-img').attr('src', srcs[srcIndex]);
         e.stopPropagation();
       });
 
       $(".btech-zoomed-image-modal-close").click(function(e) {
-        console.log("close");
         modal.remove();
         e.stopPropagation();
       });
     });
   });
-
 })();
