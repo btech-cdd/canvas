f05b52225b32d91333c87a296fb513cd4d37156f
Joshua Hveem <jhveem@btech.edu>
2024-05-08 14:29:07 -0600

    place vue html into separate file for sidebar

--- custom_features/editor_toolbar/basics.js
+++ custom_features/editor_toolbar/basics.js
@@ -274,7 +274,7 @@
 
   TOOLBAR.addButtonIcon("icon-unmuted", "Insert an information box. Can be used for warnings, examples, etc.", exampleBox);
   TOOLBAR.addButtonIcon("icon-flag", "Insert an information box. Can be used for warnings, examples, etc.", exampleBoxSmall);
-  TOOLBAR.addButtonIcon("icon-note-light", "Insert a gray callout box", calloutBox);
+  // TOOLBAR.addButtonIcon("icon-note-light", "Insert a gray callout box", calloutBox);
   TOOLBAR.addButtonIcon("icon-note-light", "Insert a gray callout box without a box shadow", calloutBoxFlat);
   TOOLBAR.addButtonIcon("icon-compose", "Insert a citation.", citation);
   TOOLBAR.addButtonIcon("icon-off", "Hide text. Reveal on mouse hover.", hideOnHover);
