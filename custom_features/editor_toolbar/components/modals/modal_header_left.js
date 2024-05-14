bcd2a5cb2e4c86822fc2a7149bec25c0eb96fbce
Joshua Hveem <jhveem@btech.edu>
2024-05-14 10:11:16 -0600

    add header to chunk

---
+++ custom_features/editor_toolbar/components/modals/modal_header_left.js
@@ -0,0 +1,55 @@
+(async function() {
+  Vue.component('rce-modal-header-left', {
+    template: ` 
+      <i
+        @click="create"
+        class="icon-text"
+        title="Create a content modal with an image on the left and text content on the right."
+      ></i>
+    `,
+    props: {
+      defaultimg: {
+        type: String,
+        default: ""
+      },
+      getContainer: {
+        type: Function,
+        default: () => {}
+      }
+    },
+    computed: {},
+    data() {
+      return {
+      } 
+    },
+    created: async function () {
+    },
+
+    methods: {
+      // CREATES A COMMENT THAT APPEARS IN THE RIGHT MARGIN (PADDING) OF THE PAGE AND MOVES TO THE TOP OF THE ASSOCIATED ELEMENT EVEN ON PAGE RESIZE
+      create: function () {
+        let editor = tinymce.activeEditor;
+        let container = this.getContainer($(editor.selection.getNode()));
+        container.prepend(`
+          <h2
+            style="
+              margin-top: -2rem;
+              background-color: ${this.color};
+              color: #FFFFFF;
+              position: relative;
+              z-index: 2;
+              font-size: 2rem;
+              display: inline-block;
+              margin-left: -2rem;
+              padding-left: 3rem;
+              width: 90%;
+              border: 0.25rem solid #FFFFFF;
+            " 
+          ><strong>HEADER</strong></h2>
+        `)
+      },
+    },
+
+    destroyed: function () {}
+  });
+})();
