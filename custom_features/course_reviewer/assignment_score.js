bdbc1c723b156964181f0fb5e5f12a638c77a57b
Joshua Hveem <jhveem@btech.edu>
2024-07-25 10:51:35 -0600

    course reviewer assignments

--- custom_features/course_reviewer/assignment_score.js
+++ custom_features/course_reviewer/assignment_score.js
@@ -1,17 +1,29 @@
 (async function () {
-  console.log("ASSIGNMENT SCORE")
-  $('#sidebar_content').css({
-    'position': 'sticky',
-    'top': 0,
-    'max-height': '100vh'
-  });
-  $("#aside").css({
-    'height': '90vh'
-  });
+  // do we have a review?
+  let assignmentReviews = [];
+  try {
+    assignmentReviews = await bridgetoolsReq('https://reports.bridgetools.dev/api/courses/586001/assignments/6280836');
+  } catch (err) {
+    console.log(err);
+  }
+  if (assignmentReviews?.length > 0) {
+    let assignmentReview = assignmentReviews[0];
+    console.log(assignmentReview);    
+    $('#sidebar_content').css({
+      'position': 'sticky',
+      'top': 0,
+      'max-height': '100vh'
+    });
+    $("#aside").css({
+      'height': '90vh'
+    });
 
-  $('#sidebar_content').append(`
-    <div>
-      <h2>Assignment Review</h2>
-    </div> 
-    `)
+    $('#sidebar_content').append(`
+      <div style="padding: 8px 0;">
+        <h2>Assignment Review</h2>
+        <div>
+        </div>
+      </div> 
+      `)
+  }
 })();
