74d34ec480b9a06a873ef50502bfb4882c15b919
Joshua Hveem <jhveem@btech.edu>
2024-07-31 14:24:45 -0600

    summary el

--- custom_features/course_reviewer/course_score.js
+++ custom_features/course_reviewer/course_score.js
@@ -161,7 +161,7 @@
       let objective = objectivesData[o];
       console.log(objective);
       let usage = (Math.round(objectivesCounts[objective.objective_id] / assignmentReviewsData.length) * 1000) / 10;
-      let topicEl = $(`<div><span style="width: 3rem;">${usage}%</span><span>${objective.objective_text.trim()}</span></div>`);
+      let topicEl = $(`<div><span style="display: inline-block; width: 3rem;">${usage}%</span><span>${objective.objective_text.trim()}</span></div>`);
       el.append(topicEl);
     }
     return el
