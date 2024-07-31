16165dc584ae3d6e1f93f29115250c6b83390d64
Joshua Hveem <jhveem@btech.edu>
2024-07-31 14:36:14 -0600

    summary el

--- custom_features/course_reviewer/course_score.js
+++ custom_features/course_reviewer/course_score.js
@@ -89,6 +89,7 @@
       assignmentCounts.chunked_content += assignment.clarity ? 1 : 0;
       assignmentCounts.career_relevance += assignment.clarity ? 1 : 0;
       assignmentCounts.provides_feedback += assignment.clarity ? 1 : 0;
+      console.log(assignment);
       assignmentCounts.clarity += assignment.clarity;
     }
     console.log(bloomsCounts);
