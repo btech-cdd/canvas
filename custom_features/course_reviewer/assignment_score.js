1b034597be3bde4424cb15ed6f68e8845937f3c2
Joshua Hveem <jhveem@btech.edu>
2024-07-31 10:33:13 -0600

    summary el

--- custom_features/course_reviewer/assignment_score.js
+++ custom_features/course_reviewer/assignment_score.js
@@ -231,7 +231,7 @@
     `);
     for (let i in assignmentReviewData.topic_tags) {
       let topic = assignmentReviewData.topic_tags[i];
-      let topicEl = $(`<span>${topic}</span>`);
+      let topicEl = $(`<span style="padding: 0.25rem; background-color: black; color: white; border-radius: 0.25rem;">${topic}</span>`);
       el.append(topicEl);
     }
     return el
