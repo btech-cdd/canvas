df71b0c6534f93b5dba31b7364e4b75f0cd6de4d
Joshua Hveem <jhveem@btech.edu>
2024-08-01 09:45:16 -0600

    change icons

--- custom_features/course_reviewer/course_score.js
+++ custom_features/course_reviewer/course_score.js
@@ -1,6 +1,6 @@
 (async function () {
   const bloomsColors = {
-    'remember': '#F58E74',
+    'remember': '#F56E74',
     'understand': '#FEB06E',
     'apply': '#FEE06E',
     'analyze': '#B1D983',
