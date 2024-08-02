0ad81262d552dac8caa3f6725b79533ab86a2a01
Joshua Hveem <jhveem@btech.edu>
2024-08-02 13:26:24 -0600

    quiz evaluator

--- custom_features/course_reviewer/quiz_score.js
+++ custom_features/course_reviewer/quiz_score.js
@@ -5,7 +5,7 @@
 // then need to pull questions that aren't in a bank separately
 */
 (async function () {
-  var questionsList = [], questionStatistics = {}, questionReviewData = {}, quizData = {};
+  var questionsList = [], questionStatistics = {}, quizReviewData = {}, quizData = {};
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
       // Generate a random index between 0 and i (inclusive)
