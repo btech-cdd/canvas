eaf290e0f2cdb91f3f5c8b2ecf66b89c00985c73
Joshua Hveem <jhveem@btech.edu>
2024-08-05 13:52:38 -0600

    list all dates

--- custom_features/modules/enrollment_dates_teacher.js
+++ custom_features/modules/enrollment_dates_teacher.js
@@ -142,7 +142,7 @@
     for (let d in dates) {
       let date = dates[d];
       console.log(date);
-      modalContent.append(`<div><span style="width: 2.5rem; display: inline-block;">${date.is_extension ? '<b>EXT</b>' : ''}</span>${date.end_date} - Created By: ${date.creator_name} At: ${date.created}</div>`)
+      modalContent.append(`<div><span style="width: 2.5rem; display: inline-block;">${date.is_extension ? '<b>EXT</b>' : ''}</span><span style="width: 10rem; display: inline-block;">${date.end_date}</span>Created By: ${date.creator_name} @ ${date.created}</div>`)
     }
     let modal = $('body .btech-modal');
     modal.on("click", function(event) {
