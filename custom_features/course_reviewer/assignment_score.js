b86fb68815a0e812f511ec4c84edb9bacd9d6e5d
Joshua Hveem <jhveem@btech.edu>
2024-08-13 14:50:59 -0600

    move get course code and year to function

--- custom_features/course_reviewer/assignment_score.js
+++ custom_features/course_reviewer/assignment_score.js
@@ -23,16 +23,9 @@
   async function refreshData() {
     courseData  = (await canvasGet(`/api/v1/courses/${ENV.COURSE_ID}`))[0];
     assignmentData = (await canvasGet(`/api/v1/courses/${ENV.COURSE_ID}/assignments/${ENV.ASSIGNMENT_ID}`))[0];
-    let regex = /^([A-Z]{4} \d{4}).*(\d{4})(?=[A-Z]{2})/;
-    let match = courseData.sis_course_id.match(regex);
-    if (match) {
-      courseCode = match[1];
-      year = match[2];
-    } else {
-      console.log("NO SIS ID FOUND");
-      courseCode = '';
-      year = '';
-    }
+    let courseCodeYear = getCourseCodeYear(courseData);
+    year = courseCodeYear.year;
+    courseCode = courseCodeYear.courseCode;
     try {
       assignmentReviewData = await bridgetoolsReq(`https://reports.bridgetools.dev/api/reviews/courses/${ENV.COURSE_ID}/assignments/${ENV.ASSIGNMENT_ID}`);
     } catch (err) {
