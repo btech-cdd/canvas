eccd0db7c989f9a414484b170a95f690539a7eb4
Joshua Hveem <JoshuaH@btech.edu>
2024-08-26 10:19:07 -0600

    add hs sections modal

--- custom_features/hs_section_adder.js
+++ custom_features/hs_section_adder.js
@@ -1,9 +1,22 @@
 (async function() {
-  let createCourseButton = $($("#content").find("[aria-label='Create new course']")[0]).parent();
-  let sectionAdderSpan = $('<span></span>');
-  let sectionAdderButton = $(createCourseButton.html().replace('Course', 'HS Sections').replace('Create new course', 'Add HS sections'));
-  sectionAdderSpan.append(sectionAdderButton);
-  createCourseButton.after(sectionAdderSpan);
+  // Get the full URL of the current page
+  const fullUrl = window.location.href;
+  const url = new URL(fullUrl);
+  // Define the regex pattern to match 'accounts/<account_id>'
+  const regex = /accounts\/(\d+)/;
+  // Execute the regex on the URL
+  const match = fullUrl.match(regex);
+  // Extract the account_id if the regex matched
+  const accountId = match ? match[1] : null;
+  // Extract the enrollment_term_id from the search parameters
+  const enrollmentTermId = url.searchParams.get('enrollment_term_id'); // "1110"
+  if (accountId != 3 && enrollmentTermId) {
+    let createCourseButton = $($("#content").find("[aria-label='Create new course']")[0]).parent();
+    let sectionAdderSpan = $('<span></span>');
+    let sectionAdderButton = $(createCourseButton.html().replace('Course', 'HS Sections').replace('Create new course', 'Add HS sections'));
+    sectionAdderSpan.append(sectionAdderButton);
+    createCourseButton.after(sectionAdderSpan);
+  }
 
   const BTECH_HS_LIST = [
     "Bear River HS AM",
@@ -38,18 +51,6 @@
   }
 
   sectionAdderButton.click(async function() {
-    $('body').append(``);
-    // Get the full URL of the current page
-    const fullUrl = window.location.href;
-    const url = new URL(fullUrl);
-    // Define the regex pattern to match 'accounts/<account_id>'
-    const regex = /accounts\/(\d+)/;
-    // Execute the regex on the URL
-    const match = fullUrl.match(regex);
-    // Extract the account_id if the regex matched
-    const accountId = match ? match[1] : null;
-    // Extract the enrollment_term_id from the search parameters
-    const enrollmentTermId = url.searchParams.get('enrollment_term_id'); // "1110"
     let modal = createModal();
     let content = $(modal.find('.btech-modal-content-inner')[0]);
     content.append("<span>COURSE</span>");
