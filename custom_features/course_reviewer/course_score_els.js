1d5c7ad434126cb9de40eb4cde5e522c8c0f75c1
Joshua Hveem <jhveem@btech.edu>
2024-09-09 14:05:47 -0600

    fixing assignment score

--- custom_features/course_reviewer/course_score_els.js
+++ custom_features/course_reviewer/course_score_els.js
@@ -108,7 +108,7 @@ async function generateDetailedContent(
   containerEl.html(`
     <div style="background-color: white; font-weight: bold; font-size: 1.5rem; padding: 0.5rem; border: 1px solid #AAA;">Course Evaluation</div>
     <div v-if="view == 'main'">
-      <div style="background-color: white; border: 1px solid #AAA; border-radius: 0.25rem; margin: 0.5rem; padding: 0.5rem;">
+      <div class="btech-course-evaluator-content-box">
         <h2>Objectives</h2>
         <div v-for="(objective, o) in objectivesData" :key="o">
           <span style="display: inline-block; width: 4rem;">{{isNaN(objective.usage) ? 0 : objective.usage}}%</span><span>{{objective.objective_text.trim()}}</span>
