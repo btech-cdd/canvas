a6137b0aaeff2497c87d8a216b47ea1fea3e6731
Joshua Hveem <jhveem@btech.edu>
2024-09-09 13:38:57 -0600

    fixing assignment score

--- custom_features/course_reviewer/course_score_els.js
+++ custom_features/course_reviewer/course_score_els.js
@@ -63,9 +63,8 @@ async function checkReviewProgress (pageCounts, quizCounts, assignmentCounts, ru
     reviewerProgressData.remaining = 100 - reviewerProgressData.processed; // Example decrement
 
 
-    console.log(reviewerProgressData);
     $('#btech-detailed-evaluation-button').html('');
-    if (course.current_update_progress <= 1 && course.current_update_progress > 0) {
+    if (course.current_update_progress <= 1 && reviewerProgressData.remaining > 0) {
       updateReviewProgress(reviewerProgressData);
     }
     let courseScore = calcCourseScore(pageCounts, quizCounts, assignmentCounts, rubricCounts);
