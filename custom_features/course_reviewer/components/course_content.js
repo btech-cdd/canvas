64e890873fc664602bcc5e23ebb8822da51ed563
Joshua Hveem <JoshuaH@btech.edu>
2024-09-27 15:14:17 -0600

    fixing course scores

--- custom_features/course_reviewer/components/course_content.js
+++ custom_features/course_reviewer/components/course_content.js
@@ -2,7 +2,7 @@
   Vue.component('course-content', {
     template: ` 
       <div style="padding: 8px 0;">
-        <h2>Assignments</h2>
+        <h2>{{ type }}</h2>
         <div v-for="criterion in criteria" :title="criterion.description">
           <span style="width: 6rem; display: inline-block;">{{ criterion.name }}</span><span>{{ calcEmoji(counts[criterion.name.toLowerCase().replace(' ', '_')] / (counts.num_reviews * 2)) }}</span>
         </div>
