f0318b81b261d27ab4ba4ae535bbad3e66db28ad
Joshua Hveem <JoshuaH@btech.edu>
2024-09-27 16:47:20 -0600

    fixing content score

--- custom_features/course_reviewer/course_scripts.js
+++ custom_features/course_reviewer/course_scripts.js
@@ -210,10 +210,13 @@ function calcCourseScore(
   let pageScore = pageCounts.num_reviews > 0 ? (calcCoursePageScore(pageCounts) * pageCounts.num_reviews) : 0;
   console.log(pageScore);
   let quizScore = quizCounts.num_reviews > 0 ? (calcCourseQuizScore(quizCounts) * quizCounts.num_reviews) : 0;
+  console.log('Quiz')
+  console.log(quizCounts);
   console.log(quizScore);
   let assignmentScore = assignmentCounts.num_reviews > 0 ? (calcCourseAssignmentScore(assignmentCounts) * assignmentCounts.num_reviews) : 0;
   console.log(assignmentScore);
   let rubricScore = rubricCounts.num_reviews > 0 ? (calcCourseRubricScore(rubricCounts) * rubricCounts.num_reviews) : 0;
+  console.log(rubricScore);
   let totalItems = quizCounts.num_reviews + assignmentCounts.num_reviews + pageCounts.num_reviews;
   score = totalItems > 0 ? (quizScore + ((assignmentScore + rubricScore) / 2) + pageScore) / totalItems : 0;
   console.log('course score');
