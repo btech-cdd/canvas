1306883318bdff26385caa82d42c6b4ba61d7aa7
Joshua Hveem <JoshuaH@btech.edu>
2024-10-02 09:49:31 -0600

    simplify criteria and reviews pulling

--- custom_features/course_reviewer/course_score.js
+++ custom_features/course_reviewer/course_score.js
@@ -67,6 +67,7 @@
     // get course level data
     courseData  = (await canvasGet(`/api/v1/courses/${ENV.COURSE_ID}`))[0];
     courseReviewData = await bridgetoolsReq(`https://reports.bridgetools.dev/api/reviews/courses/${ENV.COURSE_ID}`);
+    console.log(courseReviewData);
     criteria = await getCriteria();
     surveys = await bridgetoolsReq('https://surveys.bridgetools.dev/api/survey_data', {
         course_id: this.courseId
@@ -210,6 +211,7 @@
 
   await refreshData();
   $(document).ready(async function() {
+    console.log(courseReviewData);
     let vueApp = generateDetailedContent(
       courseReviewData
       , courseCode
