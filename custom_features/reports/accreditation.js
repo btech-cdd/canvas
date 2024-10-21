e83ff77a5baafef7660c88bcc127f3e3096d061e
Joshua Hveem <JoshuaH@btech.edu>
2024-10-21 08:20:05 -0600

    uploads not downloading

--- custom_features/reports/accreditation.js
+++ custom_features/reports/accreditation.js
@@ -405,7 +405,7 @@
               for (let i = 0; i < assignmentsData.attachments.length; i++) {
                 let attachment = assignmentsData.attachments[i];
                 console.log(attachment);
-                let iframe = await app.createIframe(attachment.url, (iframe) => {iframe.remove(); console.log(iframe);}, {});
+                let iframe = await app.createIframe(attachment.url, (iframe) => {console.log(iframe);}, {});
                 iframe.remove();
               }
             }
