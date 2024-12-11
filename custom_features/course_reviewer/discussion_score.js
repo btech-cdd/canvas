ff0ac88315de16a219473375994bd3178566d4cf
Joshua Hveem <JoshuaH@btech.edu>
2024-12-11 12:12:48 -0700

    add discussions to api

--- custom_features/course_reviewer/discussion_score.js
+++ custom_features/course_reviewer/discussion_score.js
@@ -27,7 +27,7 @@
       }
 
       courseData  = (await canvasGet(`/api/v1/courses/${ENV.course_id}`))[0];
-      discussionData = (await canvasGet(`/api/v1/courses/${ENV.course_id}/discussions/${ENV.discussion_topic_id}`))[0];
+      discussionData = (await canvasGet(`/api/v1/courses/${ENV.course_id}/discussion_topics/${ENV.discussion_topic_id}`))[0];
       let courseCodeYear = getCourseCodeYear(courseData);
       year = courseCodeYear.year;
       courseCode = courseCodeYear.courseCode;
