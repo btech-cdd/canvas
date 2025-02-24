8a8c14233bfe4ca141719017965015942316b01c
Joshua Hveem <JoshuaH@btech.edu>
2025-02-24 09:57:37 -0700

    completed courses

--- custom_features/reports/individual_page/gradesBetweenDates.js
+++ custom_features/reports/individual_page/gradesBetweenDates.js
@@ -456,6 +456,7 @@
                 courses.push(course);
             }
         });
+        console.log(courses);
         let coursesInactive = await canvasGet(`/api/v1/users/${this.userId}/courses?enrollment_Type=student&include[]=total_scores&include[]=current_grading_period_scores&include[]=term&enrollment_state=inactive&state[]=available&state[]=completed`)
         // Filter completed courses to only add those not already in `courses`
         coursesInactive.forEach(course => {
