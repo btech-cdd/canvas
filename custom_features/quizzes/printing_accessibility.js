611f26fe006b3c1d7ce4f15a4ee400ceca13f917
Joshua Hveem <JoshuaH@btech.edu>
2025-02-26 15:26:32 -0700

    add styling for printing

--- custom_features/quizzes/printing_accessibility.js
+++ custom_features/quizzes/printing_accessibility.js
@@ -1,12 +1,14 @@
 // Add CSS to hide .answer_input on print
 $(document).ready(function () {
-  $(`<style>
+   $(`<style>
     @media print { 
         .answer_input { display: none !important; } 
         div.answer_label::before { content: "⚪ "; } 
         div.move { display: none !important; }
-        .display_question { page-break-inside: avoid; border: 1px solid AAA; padding: 0.25rem; margin: 0.25rem; }
+        .display_question { page-break-inside: avoid; border: 1px solid #AAA;  padding: 0.25rem; margin: 0.25rem; }
+        .display_question .header { background-color: #EEE; padding: 0.25rem; }
       }
 
     </style>`).appendTo('head');
-  });
+
+   });
