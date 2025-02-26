d5e26e38505a94a9507af7c26ea2107ed78d9258
Joshua Hveem <JoshuaH@btech.edu>
2025-02-26 15:17:53 -0700

    add a little more styling to make printing with accessibility better

--- custom_features/quizzes/printing_accessibility.js
+++ custom_features/quizzes/printing_accessibility.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         .answer_input { display: none !important; } 
         div.answer_label::before { content: "⚪ "; } 
         div.move { display: none !important; }
-        .display_question { page-break-inside: avoid; }
+        .display_question { page-break-inside: avoid; border: 1px solid AAA; padding: 0.25rem; margin: 0.25rem; }
       }
 
     </style>`).appendTo('head');
