2558166ce2b7c164ae354fad3a58e13d80e7a98d
Joshua Hveem <JoshuaH@btech.edu>
2025-02-26 15:14:21 -0700

    add a little more styling to make printing with accessibility better

--- custom_features/quizzes/printing_accessibility.js
+++ custom_features/quizzes/printing_accessibility.js
@@ -1,10 +1,44 @@
 // Add CSS to hide .answer_input on print
-$(`<style>
-  @media print { 
-      .answer_input { display: none !important; } 
-      div.answer_label::before { content: "⚪ "; } 
-      div.move { display: none !important; }
-      .display_question { page-break-inside: avoid; }
-    }
+$(document).ready(function () {
+  $(`<style>
+    @media print { 
+        .answer_input { display: none !important; } 
+        div.answer_label::before { content: "⚪ "; } 
+        div.move { display: none !important; }
+        .display_question { page-break-inside: avoid; }
+        .question .header {
+          background-color: #AAA !important;
+        }
+      }
 
-  </style>`).appendTo('head');
+    </style>`).appendTo('head');
+
+  function getComputedStyles(selector) {
+    let styles = "";
+    $(selector).each(function () {
+      let computed = window.getComputedStyle(this);
+      let cssText = "";
+      
+      for (let i = 0; i < computed.length; i++) {
+        let prop = computed[i];
+        cssText += `${prop}: ${computed.getPropertyValue(prop)}; `;
+      }
+
+      styles += `${selector} { ${cssText} } `;
+    });
+    return styles;
+  }
+
+  let headerStyles = getComputedStyles(".question .header");
+
+  let printStyles = `
+    <style>
+      @media print {
+        ${headerStyles}
+      }
+    </style>
+  `;
+    console.log(printStyles);
+
+  $("head").append(printStyles);
+});
