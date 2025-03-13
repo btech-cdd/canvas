ed0d810dc9ff610a533acc83406a018e8ccdcef4
Joshua Hveem <JoshuaH@btech.edu>
2025-03-13 09:20:03 -0600

    add btech-callout-header

--- custom_features/editor_toolbar/basics.js
+++ custom_features/editor_toolbar/basics.js
@@ -275,7 +275,7 @@
     `);
 
   TOOLBAR.addButtonIcon("icon-note-light icon-Solid", "Callout Box with Colored Title", "Insert a callout box with a colored header.", exampleBox);
-  $('[aria-label="Callout Box with Colored Title"]').css('color', '#d22232');
+  $('[aria-label="Callout Box with Colored Title"]').css('color', '#B20B0F');
 
   TOOLBAR.addButtonIcon("icon-note-light", "Callout Box Gray. Light Border.", "Insert a gray callout box with light border. Designed for on white backgrounds.", calloutBox);
   TOOLBAR.addButtonIcon("icon-note-light icon-Solid", "Callout Box Gray. Dark Border.", "Insert a gray callout box with dark border. Designed for on gray backgrounds.", calloutBoxGrayonGray);
