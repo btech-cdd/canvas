1fd394f2f550686ade2777ae3a7f8978ae52d8a8
Joshua Hveem <Josh.Hveem@btech.edu>
2025-03-26 19:46:28 -0600

    update

--- custom_features/modules/course_features.js
+++ custom_features/modules/course_features.js
@@ -87,7 +87,7 @@
                   top:0;
                   left:0;
                   width:100%;
-                  height:100%;
+                  height:100px;
                   object-fit:cover;
                   z-index:0;
                 ">
