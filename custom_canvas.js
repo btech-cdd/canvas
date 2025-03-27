b9ceeff580b4f58043159f9ded5b7c3564f9abc3
Joshua Hveem <Josh.Hveem@btech.edu>
2025-03-26 19:54:19 -0600

    update

--- custom_canvas.js
+++ custom_canvas.js
@@ -89,7 +89,7 @@ var MONTH_NAMES_SHORT = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug
               <img src="https://bridgetools.dev/canvas/media/course-banners/${ENV.ACCOUNT_ID}.png" style="
                 position:absolute;
                 top:0;
-                left: calc(100% - 250px);
+                left: calc(100% - 300px);
                 width:250px;
                 height:100%;
                 object-fit:cover;
